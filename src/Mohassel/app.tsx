f4372541f0ba2650119220cc75d3ad5874426711
Ahmed Fahmy <afahmy31@gmail.com>
2020-03-11 12:11:10 +0200

    initialize multi app react projects for mohassil app

---
+++ src/Mohassel/app.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import SharedComponent from '../Shared/test';
+
+const App = () => {
+    return (
+        <>
+            <SharedComponent></SharedComponent>
+            <h1>hi mohassel</h1>
+        </>
+    )
+};
+
+export default App;
