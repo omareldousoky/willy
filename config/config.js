6436f24f0a5050b61c2c7784f5229b068f55a91f
Ahmed Fahmy <afahmy31@gmail.com>
2020-03-19 18:40:58 +0200

    fix env variabls for prod

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    REACT_APP_BASE_URL: process.env.REACT_APP_BASE_URL || '',
+    REACT_APP_MOHASSEL_URL: process.env.REACT_APP_MOHASSEL_URL || '',
+    REACT_APP_LOGIN_URL: process.env.REACT_APP_LOGIN_URL || '',
+    REACT_APP_GOOGLE_MAP_KEY: process.env.REACT_APP_GOOGLE_MAP_KEY || '',
+}
+
+module.exports = config;
