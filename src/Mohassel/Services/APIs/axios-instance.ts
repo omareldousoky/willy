20c4761f5896252ff84ab718afe313aecf10c55e
Omar Eldousoky <omareldousoky@gmail.com>
2020-03-19 13:55:47 +0200

    removed unnecessary console logs and added formik validators to login

--- src/Mohassel/Services/APIs/axios-instance.ts
+++ src/Mohassel/Services/APIs/axios-instance.ts
@@ -1,5 +1,19 @@
 import axiosLib from 'axios';
 
+// function getCookie(cname: string) {
+//     const name = cname + "=";
+//     const ca = document.cookie.split(';');
+//     for (let i = 0; i < ca.length; i++) {
+//       let c = ca[i];
+//       while (c.charAt(0) == ' ') {
+//         c = c.substring(1);
+//       }
+//       if (c.indexOf(name) == 0) {
+//         return c.substring(name.length, c.length);
+//       }
+//     }
+//     return "";
+//   }
 function errorResponseHandler(error: any) {
 
     // check for errorHandle config
