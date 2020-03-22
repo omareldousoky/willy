445738af62060cf495d10f9c09750232f4ce4331
Ahmed Fahmy <afahmy31@gmail.com>
2020-03-22 11:40:45 +0200

    remove tolowercase from token

--- src/Mohassel/Services/APIs/axios-instance.ts
+++ src/Mohassel/Services/APIs/axios-instance.ts
@@ -23,7 +23,7 @@ function getCookie(cookie: string) {
     const allCookies = document.cookie.split(';');
   for (let i = 0; i < allCookies.length; i++) {
     const name = allCookies[i].split('=')[0].toLowerCase().trim();
-    const value = allCookies[i].split('=')[1].toLowerCase().trim();
+    const value = allCookies[i].split('=')[1].trim();
     if (name === cookie) {
       return value;
     } else if (value === cookie) {
