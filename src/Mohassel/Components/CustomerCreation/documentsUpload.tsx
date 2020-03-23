b24a3478b3834692f5fe7c4fa872fe349e203432
Ahmed Fahmy <afahmy31@gmail.com>
2020-03-23 11:00:10 +0200

    checking out

--- src/Mohassel/Components/CustomerCreation/documentsUpload.tsx
+++ src/Mohassel/Components/CustomerCreation/documentsUpload.tsx
@@ -7,9 +7,31 @@ import * as local from '../../../Shared/Assets/ar.json';
 
 export const DocumentsUpload = (props: any) => {
     const { values, handleSubmit, handleBlur, handleChange, errors, touched, setFieldValue, previousStep } = props;
-    return(
+    return (
         <Form>
-            <h1>HI</h1>
+            {/* <div
+                style={{
+                    borderRadius: 15,
+                    border: 'dashed grey 2px',
+                    backgroundColor: 'rgba(255,255,255,.8)',
+                    display: 'flex',
+                    width: 300,
+                    height: 300,
+                    margin: 'auto'
+                }}
+            >
+                <div
+                    style={{
+                        margin: 'auto',
+                        textAlign: 'center',
+                        color: 'grey',
+                        fontSize: 36
+                    }}
+                >
+                    <div>drop here :)</div>
+                </div>
+            </div> */}
+            
         </Form>
     )
 }
