5caa11ee2271befd8524650963e268abee42ffd8
Ahmed Fahmy <afahmy31@gmail.com>
2020-03-30 15:44:58 +0200

    fix error responses

--- src/Mohassel/Services/APIs/axios-instance.ts
+++ src/Mohassel/Services/APIs/axios-instance.ts
@@ -9,7 +9,7 @@ function errorResponseHandler(error: any) {
     }
     // if has response show the error
     switch (error.response.status) {
-        case 400:
+        // case 400:
         case 401:
             document.cookie = "token=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
             window.location.href = process.env.REACT_APP_LOGIN_URL || '';
