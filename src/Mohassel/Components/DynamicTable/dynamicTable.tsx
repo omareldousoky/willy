e6bdf71f5aabe403632bdbf8bae177813c13f612
Ahmed Fahmy <afahmy31@gmail.com>
2020-05-20 11:16:31 +0200

    fix pagination long numbers

--- src/Mohassel/Components/DynamicTable/dynamicTable.tsx
+++ src/Mohassel/Components/DynamicTable/dynamicTable.tsx
@@ -19,6 +19,14 @@ const DynamicTable = (props: Props) => {
   for (let index = 1; index <= Math.ceil(props.totalCount / rowsPerPage); index++) {
     totalPages.push(index)
   }
+  function getArrayOfNumbers() {
+    const length = page + 5 >= totalPages.length? totalPages.length: page +5;
+    const output: Array<number> = [];
+    for (let index = page + 1; index <= length; index++) {
+      output.push(index)
+    }
+    return output;
+  }
   return (
     <>
       <Table striped hover style={{ textAlign: 'right' }}>
@@ -48,7 +56,7 @@ const DynamicTable = (props: Props) => {
         </tbody>
       </Table>
       {props.pagination ?
-        <div className="footer-container">
+        <div className="footer-container" style={{marginBottom: 200}}>
           <div className="dropdown-container">
             <p className="dropdown-label">{local.show}</p>
             <Form.Control as="select" className="dropdown-select" onChange={(event) => {
@@ -69,7 +77,8 @@ const DynamicTable = (props: Props) => {
                   props.changeNumber && props.changeNumber('from', (page * rowsPerPage - rowsPerPage));
                 }
               }}>{local.previous}</div>
-            {totalPages.map(number => {
+              <div className="pagination-numbers">
+            {getArrayOfNumbers().map(number => {
               return (
                 <div key={number}
                   className={page === number - 1 ? "pagination-number-active" : "pagination-number-inactive"}
@@ -81,6 +90,7 @@ const DynamicTable = (props: Props) => {
                 </div>
               )
             })}
+            </div>
             <div className={page + 1 !== Math.ceil(props.totalCount / rowsPerPage) ? "pagination-next-prev-enabled" : "pagination-next-prev-disabled"}
               onClick={() => {
                 if (page + 1 !== Math.ceil(props.totalCount / rowsPerPage)) {
