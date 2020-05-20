dd61b44ef1ba6e4b3a9ed37f1997e77cbc68a7e6
Sara Alaa <sara3laa.93@gmail.com>
2020-05-20 03:54:15 +0200

    phone only numbers

--- src/Mohassel/Components/BranchCreation/branchCreationInitialState.ts
+++ src/Mohassel/Components/BranchCreation/branchCreationInitialState.ts
@@ -16,7 +16,7 @@ export const step1: BasicValues = {
 export const branchCreationValidationStepOne = Yup.object().shape({
     name: Yup.string().trim().matches(/^[^a-z]*[a-z].*$/,local.branchNameInavlid).max(150, local.maxLength150).required(local.required),
     governorate: Yup.string().trim().matches(/^[a-zA-Z ]*$/,local.governorateNameInavlid).max(150,local.maxLength150).required(local.required),
-    phoneNumber: Yup.string().min(11, local.minLength11).max(11,local.maxLength11),
+    phoneNumber: Yup.string().matches(/^[0-9]*$/,local.onlyNumbers).max(11,local.maxLength11),
     faxNumber: Yup.string().min(10, local.minLength10).max(11,local.maxLength11),
     address: Yup.string().trim().max(150, local.maxLength150).required(local.required),
     postalCode: Yup.string().trim().matches(/^[0-9]*$/,local.onlyNumbers).max(5,local.maxLength5),
