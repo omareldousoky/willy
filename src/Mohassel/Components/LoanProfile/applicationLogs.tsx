98fb7031004f35865a9feaa8335727266fa3c2bc
Omar Eldousoky <omareldousoky@gmail.com>
2020-05-20 14:20:26 +0200

    render logs for app

--- src/Mohassel/Components/LoanProfile/applicationLogs.tsx
+++ src/Mohassel/Components/LoanProfile/applicationLogs.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Loader } from '../../../Shared/Components/Loader';
 import { getApplicationLogs } from '../../Services/APIs/loanApplication/applicationLogs';
-
+import DynamicTable from '../DynamicTable/dynamicTable';
+import * as local from '../../../Shared/Assets/ar.json';
+import { getRenderDate } from '../../Services/getRenderDate';
 interface Props {
     id: string;
 }
@@ -10,7 +12,38 @@ interface State {
     loading: boolean;
     data: any;
 }
-
+const mappers = [
+    {
+      title: local.action,
+      key: "action",
+      render: data => data.action
+    },
+    {
+      title: local.author,
+      key: "authorName",
+      render: data => data.authorName
+    },
+    {
+      title: local.authorId,
+      key: "authorId",
+      render: data => data.authorId
+    },
+    {
+      title: local.createdAt,
+      key: "createdAt",
+      render: data => getRenderDate(data.createdAt)
+    },
+    // {
+    //   title: local.customerId,
+    //   key: "customerId",
+    //   render: data => data.customerId
+    // },
+    // {
+    //     title: local.customerBranchId,
+    //     key: "customerBranchId",
+    //     render: data => data.customerBranchId
+    //   },
+  ]
 class Logs extends Component<Props, State> {
     constructor(props) {
         super(props);
@@ -26,20 +59,31 @@ class Logs extends Component<Props, State> {
         this.setState({ loading: true })
         const res = await getApplicationLogs(id);
         if (res.status === "success") {
-          this.setState({
-            data: res.body.applications,
-            loading: false
-          })
+            this.setState({
+                data: res.body.data,
+                loading: false
+            })
         } else {
-          console.log("error")
-          this.setState({ loading: false })
+            console.log("error")
+            this.setState({ loading: false })
         }
     }
     render() {
         return (
             <>
                 <Loader type="fullsection" open={this.state.loading} />
+                {this.state.data.length > 0 &&
+                    <DynamicTable totalCount={0} pagination={false} data={this.state.data} mappers={mappers} />
 
+                // this.state.data.map((log, i) =>
+                // <div key={i} className='d-flex justify-content-between'>
+                //     <p>{log.action}</p>
+                //     <p>{log.authorId}</p>
+                //     <p>{log.authorName}</p>
+                //     <p>{log.createdAt}</p>
+                //     <p>{log.customerId}</p>
+                // </div>)
+                }
             </>
         )
     }
