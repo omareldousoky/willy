02009ed70abf3f655037119b3e87d90c00a27528
Sara Alaa <sara3laa.93@gmail.com>
2020-05-21 16:30:10 +0200

    fix-edit

--- src/Mohassel/Services/APIs/User-Creation/editUser.ts
+++ src/Mohassel/Services/APIs/User-Creation/editUser.ts
@@ -1,7 +1,7 @@
 import axios from '../axios-instance';
 
 export const editUser = async (data: object,id: string) => {
-    const url = process.env.REACT_APP_BASE_URL + `/user${id}`;
+    const url = process.env.REACT_APP_BASE_URL + `/user/${id}`;
     try {
         const res = await axios.put(url, data);
         return { status: "success", body: res.data }
