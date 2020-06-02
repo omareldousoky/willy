43e39b4649afd377f62bd9c682a5568524afa4c8
Omar Eldousoky <omareldousoky@gmail.com>
2020-06-02 13:35:46 +0200

    typo fix

--- src/Mohassel/Components/NavBar/navBar.tsx
+++ src/Mohassel/Components/NavBar/navBar.tsx
@@ -118,7 +118,7 @@ class NavBar extends Component<Props, State> {
             document.cookie = "roles=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
             document.cookie = "validbranches=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
             document.cookie = "clientpermissions=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-            document.cookie = "selectebranch=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+            document.cookie = "selectedbranch=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
             window.location.href = process.env.REACT_APP_LOGIN_URL || '';
           }}>{local.logOut}</Button>
         </div>
