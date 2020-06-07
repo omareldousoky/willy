701789beb23b451dc9e57014ed09a1e708d1457d
Ahmed Fahmy <afahmy31@gmail.com>
2020-06-07 17:02:15 +0200

    finish loan contract pdf

--- src/Mohassel/Components/Landing/landing.tsx
+++ src/Mohassel/Components/Landing/landing.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Can from '../../config/Can';
 import * as local from '../../../Shared/Assets/ar.json';
-import CashReceipt from '../pdfTemplates/cashReceipt/cashReceipt';
+import LoanContract from '../pdfTemplates/loanContract/loanContract';
 export const Landing = () => {
     return (
         <>
         <h1 style={{textAlign: 'right', margin: 20}}>{local.welcomeToMohassel}</h1>
-        <CashReceipt></CashReceipt>
+        <LoanContract></LoanContract>
         </>
     )
 }
