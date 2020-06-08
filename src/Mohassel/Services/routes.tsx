19d0ad6da04da1187625cff2b32216714d79e5b5
Sara Alaa <sara3laa.93@gmail.com>
2020-06-08 03:30:48 +0200

    setup-routes

---
+++ src/Mohassel/Services/routes.tsx
@@ -0,0 +1,108 @@
+
+import * as React from 'react';
+import CustomerCreation from '../Components/CustomerCreation/customer-creation';
+import UserCreation from '../Components/UserCreation/user-creation';
+import FormulaCreation from '../Components/LoanFormulaCreation/loanFormulaCreation';
+import FormulaTest from '../Components/LoanFormulaCreation/loanFormulaTest';
+import LoanProductCreation from '../Components/LoanProductCreation/loanProductCreation';
+import LoanApplicationCreation from '../Components/LoanApplication/loanApplicationCreation';
+import AssignProductToBranch from '../Components/Branch/assignProductToBranch';
+import TrackLoanApplications from '../Components/TrackLoanApplications/trackLoanApplications';
+import LoanCreation from '../Components/LoanCreation/loanCreation';
+import NavBar from '../Components/NavBar/navBar';
+import LoanUses from '../Components/LoanUses/loanUses';
+import BulkApplicationApproval from '../Components/BulkApplicationApproval/bulkApplicationApproval';
+import ManageAccounts from '../Components/ManageAccounts/manageAccounts';
+import LoanProfile from '../Components/LoanProfile/loanProfile';
+import LoanList from '../Components/LoanList/loanList';
+import RoleCreation from '../Components/Roles/roleCreation';
+import RoleProfile from '../Components/Roles/roleProfile';
+import { Landing } from '../Components/Landing/landing';
+import { getCookie } from './getCookie';
+import Can from '../config/Can';
+import UserDetails from '../Components/userDetails/user-details';
+import CreateBranch from '../Components/BranchCreation/create-branch';
+import CustomersList from '../Components/CustomerCreation/customersList';
+import ability from '../config/ability';
+import * as local from '../../Shared/Assets/ar.json';
+
+export const routes = [
+{
+  path: "/",
+  component: Landing,
+} , {
+    path: "/customers",
+    label: local.customers,
+render: (props)=> {<Can I='getCustomer' a='customer'><CustomersList {...props} /></Can>},
+    routes: [
+      {
+      path: "/new-customer",
+      label: local.newCustomer,
+      render: (props) => {<Can I='createCustomer' a='customer'><CustomerCreation {...props} edit={false} /></Can>} , 
+    },
+    {
+      path: "/edit-customer",
+      label: local.editCustomer,
+      render: (props) => {<Can I='updateCustomer' a='customer'><CustomerCreation {...props} edit={true} /> </Can>},
+    }
+  ]
+},
+{
+  path: "/new-formula",
+  label: local.createCalculationMethod,
+  render: (props) => {<Can I='createCalculationFormula' a='product'><FormulaCreation /></Can>},
+},
+{
+  path: "/test-formula",
+  render : (props) => {<Can I='testCalculate' a='product'><FormulaTest {...props} /></Can>},
+}, 
+{
+  path: "/new-loan-product",
+  label: local.createLoanProduct,
+  render : (props) => {<Can I='createLoanProduct' a='product'><LoanProductCreation /></Can>},
+},
+ {
+  path: "/track-loan-applications",
+  label: local.loanApplications,
+  render: (props) => {<Can I='getLoanApplication' a='application'><TrackLoanApplications /></Can>},
+  routes: [
+    {
+      path: "/edit-loan-application",
+      label: local.editLoan ,
+      render: (props) => {(props) => <LoanApplicationCreation {...props} edit={true} />},
+    } ,
+    {
+      path: "/create-loan",
+      label: local.createLoan, 
+      render: (props) => {(ability.can( 'createLoan', 'application') || ability.can('issueLoan','application'))?<LoanCreation {...props} /> : null},
+    } , 
+    {
+      path: "/loan-profile",
+      label: local.loanDetails,
+      render: (props) => {(ability.can('getLoanApplication','application') || ability.can('getIssuedLoan','application'))?<LoanProfile {...props} />:null},
+
+    }
+]
+} , 
+{
+  path: "/loan-uses" ,
+  label: local.loanUses,
+  render: (props) => {<Can I='loanUsage' a='config'><LoanUses /></Can>}
+},
+{
+  path: "/bulk-approvals",
+  label: local.bulkLoanApplicationsApproval,
+  render: (props) => {<Can I='approveLoanApplication' a='application'> <BulkApplicationApproval /></Can>}
+}, 
+{
+   path: "/manage-accounts",
+   label: local.manageAccounts,
+   render: (props) => {<ManageAccounts />},
+
+}, {
+  path: "/loans",
+  label: local.issuedLoans,
+  render: (props) => {<Can I='getIssuedLoan' a='application'> <LoanList {...props} /></Can>}
+
+}
+];
--- src/Mohassel/Services/utils.ts
+++ src/Mohassel/Services/utils.ts
@@ -20,3 +20,54 @@ return [year, month, day].join('-');
   
   } else  return '';
 }
+
+
+export const combinePaths = (parent, child) =>
+  `${parent.replace(/\/$/, "")}/${child.replace(/^\//, "")}`;
+
+export const buildPaths = (routes, parentPath = "") =>
+  routes.map(route => {
+    const path = combinePaths(parentPath, route.path);
+
+    return {
+      ...route,
+      path,
+      ...(route.routes && { routes: buildPaths(route.routes, path) })
+    };
+  });
+
+
+export const setupParents = (routes, parentRoute = {}) =>
+  routes.map(route => {
+    const withParent = {
+      ...route,
+      ...(parentRoute && { parent: parentRoute })
+    };
+
+    return {
+      ...withParent,
+      ...(withParent.routes && {
+        routes: setupParents(withParent.routes, withParent)
+      })
+    };
+  });
+
+
+export const flattenRoutes = routes =>
+  routes
+    .map(route => [route.routes ? flattenRoutes(route.routes) : [], route])
+    .flat(Infinity);
+
+
+export const generateAppRoutes = routes => {
+  return flattenRoutes(setupParents(buildPaths(routes)));
+};
+
+
+export const pathTo = route => {
+  if (!route.parent) {
+    return [route];
+  }
+
+  return [...pathTo(route.parent), route];
+};
