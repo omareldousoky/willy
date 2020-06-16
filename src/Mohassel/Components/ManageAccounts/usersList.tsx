0aebf643016c9fa4fafff2c6f793ca98fbe6144a
Ahmed Fahmy <afahmy31@gmail.com>
2020-06-16 16:42:36 +0200

    fix navbar and search bugs

--- src/Mohassel/Components/ManageAccounts/usersList.tsx
+++ src/Mohassel/Components/ManageAccounts/usersList.tsx
@@ -129,7 +129,7 @@ class UsersList extends Component<Props, State> {
               </div>
             </div>
             <hr className="dashed-line" />
-            <Search searchKeys={['keyword', 'dateFromTo', 'employment']} dropDownKeys={['name', 'nationalId']} url="user" from={this.state.from} size={this.state.size} hqBranchIdRequest={this.props.branchId} />
+            <Search searchKeys={['keyword', 'dateFromTo']} dropDownKeys={['name', 'nationalId']} url="user" from={this.state.from} size={this.state.size} hqBranchIdRequest={this.props.branchId} />
 
             <DynamicTable
               totalCount={this.props.totalCount}
