e6989510173e5ff89fc2c28ae432e064b468e82d
Ahmed Fahmy <afahmy31@gmail.com>
2020-06-16 14:28:46 +0200

    add dropdown lists to search component

--- src/Mohassel/Components/CustomerCreation/customersList.tsx
+++ src/Mohassel/Components/CustomerCreation/customersList.tsx
@@ -96,7 +96,7 @@ class CustomersList extends Component<Props, State> {
             </div>
           </div>
           <hr className="dashed-line" />
-          <Search searchKeys={['keyword', 'dateFromTo', 'governorate']} url="customer" from={this.state.from} size={this.state.size}  hqBranchIdRequest = {this.props.branchId}/>
+          <Search searchKeys={['keyword', 'dateFromTo', 'governorate']} dropDownKeys={['name', 'nationalId', 'code']} url="customer" from={this.state.from} size={this.state.size}  hqBranchIdRequest = {this.props.branchId}/>
           {this.props.data &&
             <DynamicTable
               totalCount={this.props.totalCount}
--- src/Mohassel/Components/Search/search.tsx
+++ src/Mohassel/Components/Search/search.tsx
@@ -7,6 +7,9 @@ import * as local from '../../../Shared/Assets/ar.json';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 import { search, searchFilters } from '../../redux/search/actions';
 import { BranchesDropDown } from '../dropDowns/allDropDowns';
 import { parseJwt } from '../../Services/utils';
@@ -29,18 +32,21 @@ interface Props {
   roleId?: string;
   hqBranchIdRequest?: string;
   searchKeys: Array<string>;
+  dropDownKeys?: Array<string>;
   search: (data) => void;
   searchFilters: (data) => void;
   setLoading: (data) => void;
 }
 interface State {
   governorates: Array<any>;
+  dropDownValue: string;
 }
 class Search extends Component<Props, State> {
   constructor(props) {
     super(props);
     this.state = {
       governorates: [],
+      dropDownValue: 'name'
     }
   }
   componentDidMount() {
@@ -60,7 +66,8 @@ class Search extends Component<Props, State> {
     }
   }
   submit = async (values) => {
-    const obj = { ...values, ...{ from: this.props.from } };
+    const obj = { ...values, ...{ from: this.props.from } , [this.state.dropDownValue]: values.name? values.name: values[this.state.dropDownValue]};
+    if(this.state.dropDownValue !== 'name') delete obj.name;
     if (obj.hasOwnProperty('fromDate'))
       obj.fromDate = new Date(obj.fromDate).setHours(0, 0, 0, 0).valueOf();
     if (obj.hasOwnProperty('toDate'))
@@ -69,7 +76,7 @@ class Search extends Component<Props, State> {
       obj.roleId = this.props.roleId;
     obj.from = 0;
     this.props.searchFilters(obj);
-    this.props.search({ ...obj, size: this.props.size, url: this.props.url , branchId: this.props.hqBranchIdRequest  })
+    this.props.search({ ...obj, size: this.props.size, url: this.props.url, branchId: this.props.hqBranchIdRequest })
   }
   getInitialState() {
     const initialState: InitialFormikState = {};
@@ -97,6 +104,14 @@ class Search extends Component<Props, State> {
       else return false;
     } else return true;
   }
+  getArValue(key: string){
+    switch(key) {
+      case 'name': return local.name;
+      case 'nationalId': return local.nationalId;
+      case 'code': return local.customerId;
+      default: return '';
+    }
+  }
   render() {
     return (
       <Formik
@@ -113,11 +128,11 @@ class Search extends Component<Props, State> {
                 if (searchKey === 'keyword') {
                   return (
                     <Col key={index} sm={6}>
-                      <InputGroup style={{ direction: 'ltr', flex: 1, marginLeft: 20 }}>
-                        <Form.Control
+                      <InputGroup style={{ direction: 'ltr' }}>
+                        <FormControl
                           type="text"
-                          name="name"
-                          data-qc="name"
+                          name={this.state.dropDownValue}
+                          data-qc="searchKeyword"
                           onChange={formikProps.handleChange}
                           style={{ direction: 'rtl', borderRight: 0, padding: 22 }}
                           placeholder={local.searchByNameOrNationalId}
@@ -125,6 +140,19 @@ class Search extends Component<Props, State> {
                         <InputGroup.Append>
                           <InputGroup.Text style={{ background: '#fff' }}><span className="fa fa-search fa-rotate-90"></span></InputGroup.Text>
                         </InputGroup.Append>
+                        {this.props.dropDownKeys && this.props.dropDownKeys.length?
+                        <DropdownButton
+                          as={InputGroup.Append}
+                          variant="outline-secondary"
+                          title={this.getArValue(this.state.dropDownValue)}
+                          id="input-group-dropdown-2"
+                          data-qc="search-dropdown"
+                        >
+                          {this.props.dropDownKeys.map((key, index) =>
+                            <Dropdown.Item key={index} data-qc={key} onClick={() => this.setState({dropDownValue: key})}>{this.getArValue(key)}</Dropdown.Item>
+                            )}
+                        </DropdownButton>
+                        : null }
                       </InputGroup>
                     </Col>
                   );
