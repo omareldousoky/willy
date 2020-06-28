009c14af280b94e276e9140eb354926af2b12660
Omar Eldousoky <omareldousoky@gmail.com>
2020-06-28 15:21:30 +0200

    range validation for noOfInstallmentss

--- src/Mohassel/Components/LoanProductCreation/loanProductStates.ts
+++ src/Mohassel/Components/LoanProductCreation/loanProductStates.ts
@@ -5,7 +5,7 @@ export const LoanProductValidation = Yup.object().shape({
     beneficiaryType: Yup.string().required('required!'),
     calculationFormulaId: Yup.string().required('required!'),
     periodLength: Yup.number().integer('Must be int').min(1, "Can't be less than 1").required('required!'),
-    noOfInstallments: Yup.number().integer('Must be int').min(0, "Can't be less than 0").test("noOfInstallments", `cant be out of range` + Yup.ref('minInstallment') + 'to' + Yup.ref('maxInstallment'),
+    noOfInstallments: Yup.number().integer('Must be int').min(0, "Can't be less than 0").test("noOfInstallments", `outOfRange`,
         function (this: any, value: any) {
             const { minInstallment, maxInstallment } = this.parent
             if (minInstallment === 0 && maxInstallment === 0) {
