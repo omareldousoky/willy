6c5ba380337ea5662e1ea666f74405fb411fdfc5
Ahmed Fahmy <afahmy31@gmail.com>
2020-07-05 18:50:51 +0200

    checking out

--- src/Mohassel/Components/LoanCreation/loanCreation.tsx
+++ src/Mohassel/Components/LoanCreation/loanCreation.tsx
@@ -201,7 +201,7 @@ class LoanCreation extends Component<Props, State> {
             </tr>
           </thead>
           <tbody>
-            {this.state.installmentsData.installments && this.state.installmentsData.installments.map((installment, index) => {
+            {this.state.installmentsData.installments && this.state.installmentsData.installments.sort(function(a,b) {return a.installmentNumber - b.installmentNumber }).map((installment, index) => {
               return (
                 <tr key={index}>
                   <td>{installment.id}</td>
@@ -232,7 +232,6 @@ class LoanCreation extends Component<Props, State> {
                       type="date"
                       name="loanCreationDate"
                       data-qc="loanCreationDate"
-                      value={formikProps.values.loanCreationDate}
                       onChange={(e)=> {
                         formikProps.setFieldValue('loanCreationDate', e.currentTarget.value);
                         this.handleCreationDateChange(e.currentTarget.value);
