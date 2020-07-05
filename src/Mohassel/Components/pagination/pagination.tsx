2fe4a0cf56017a701bbd41d222d57cbc4498d4dc
Sara Alaa <sara3laa.93@gmail.com>
2020-07-05 21:47:14 +0200

    remove logs

--- src/Mohassel/Components/pagination/pagination.tsx
+++ src/Mohassel/Components/pagination/pagination.tsx
@@ -1,4 +1,4 @@
-import React , {useState}from 'react';
+import React, { useState } from 'react';
 import * as local from '../../../Shared/Assets/ar.json';
 import Form from 'react-bootstrap/Form';
 const Pagination = props => {
@@ -6,63 +6,63 @@ const Pagination = props => {
     const [rowsPerPage, changeRowsPerPage] = useState(props.pagination ? 5 : props.dataLength);
     const totalPages: Array<number> = [];
     for (let index = 1; index <= Math.ceil(props.totalCount / rowsPerPage); index++) {
-      totalPages.push(index)
+        totalPages.push(index)
     }
     function getArrayOfNumbers() {
         const length = page + 5 >= totalPages.length ? totalPages.length : page + 5;
         const output: Array<number> = [];
         for (let index = page + 1; index <= length; index++) {
-          output.push(index)
+            output.push(index)
         }
         return output;
-      }
+    }
     return (
         <>
-        <div className="footer-container" style={{ marginBottom: 20, marginRight: 30 }}>
-          <div className="dropdown-container">
-            <p className="dropdown-label">{local.show}</p>
-            <Form.Control as="select" className="dropdown-select" onChange={(event) => {
-              changeRowsPerPage(Number(event.currentTarget.value));
-              props.changeNumber && props.changeNumber('size', Number(event.currentTarget.value))
-              props.changeNumber && props.changeNumber('from', 0)
-              changePage(0)
-            }}>
-              <option value={5}>5</option>
-              <option value={10}>10</option>
-            </Form.Control>
-          </div>
-          <div className="pagination-container">
-            <div className={page === 0 ? "pagination-next-prev-disabled" : "pagination-next-prev-enabled"}
-              onClick={() => {
-                if (page !== 0) {
-                  changePage(page - 1);
-                  props.changeNumber && props.changeNumber('from', (page * rowsPerPage - rowsPerPage));
-                }
-              }}>{local.previous}</div>
-            <div className="pagination-numbers">
-              {getArrayOfNumbers().map(number => {
-                return (
-                  <div key={number}
-                    className={page === number - 1 ? "pagination-number-active" : "pagination-number-inactive"}
-                    onClick={() => {
-                      changePage(number - 1);
-                      props.changeNumber && props.changeNumber('from', (number - 1) * rowsPerPage)
+            <div className="footer-container" style={{ marginBottom: 20, marginRight: 30 }}>
+                <div className="dropdown-container">
+                    <p className="dropdown-label">{local.show}</p>
+                    <Form.Control as="select" className="dropdown-select" onChange={(event) => {
+                        changeRowsPerPage(Number(event.currentTarget.value));
+                        props.changeNumber && props.changeNumber('size', Number(event.currentTarget.value))
+                        props.changeNumber && props.changeNumber('from', 0)
+                        changePage(0)
                     }}>
-                    <p>{number}</p>
-                  </div>
-                )
-              })}
+                        <option value={5}>5</option>
+                        <option value={10}>10</option>
+                    </Form.Control>
+                </div>
+                <div className="pagination-container">
+                    <div className={page === 0 ? "pagination-next-prev-disabled" : "pagination-next-prev-enabled"}
+                        onClick={() => {
+                            if (page !== 0) {
+                                changePage(page - 1);
+                                props.changeNumber && props.changeNumber('from', (page * rowsPerPage - rowsPerPage));
+                            }
+                        }}>{local.previous}</div>
+                    <div className="pagination-numbers">
+                        {getArrayOfNumbers().map(number => {
+                            return (
+                                <div key={number}
+                                    className={page === number - 1 ? "pagination-number-active" : "pagination-number-inactive"}
+                                    onClick={() => {
+                                        changePage(number - 1);
+                                        props.changeNumber && props.changeNumber('from', (number - 1) * rowsPerPage)
+                                    }}>
+                                    <p>{number}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className={page + 1 !== Math.ceil(props.totalCount / rowsPerPage) ? "pagination-next-prev-enabled" : "pagination-next-prev-disabled"}
+                        onClick={() => {
+                            if (page + 1 !== Math.ceil(props.totalCount / rowsPerPage)) {
+                                changePage(page + 1);
+                                props.changeNumber && props.changeNumber('from', (page * rowsPerPage + rowsPerPage));
+                            }
+                        }}>{local.next}</div>
+                </div>
             </div>
-            <div className={page + 1 !== Math.ceil(props.totalCount / rowsPerPage) ? "pagination-next-prev-enabled" : "pagination-next-prev-disabled"}
-              onClick={() => {
-                if (page + 1 !== Math.ceil(props.totalCount / rowsPerPage)) {
-                  changePage(page + 1);
-                  props.changeNumber && props.changeNumber('from', (page * rowsPerPage + rowsPerPage));
-                }
-              }}>{local.next}</div>
-          </div>
-        </div>
-  </>
+        </>
     )
 }
 
--- src/Mohassel/Components/userDetails/customersForUser.tsx
+++ src/Mohassel/Components/userDetails/customersForUser.tsx
@@ -57,9 +57,8 @@ class CustomersForUser extends Component<Props, State> {
     this.getCustomersForUser();
   }
   async getCustomersForUser(name?: string) {
-    console.log('name',name)
     this.setState({ loading: true })
-    const res = await searchCustomer({ name: name ,size: this.state.size, from: this.state.from,representativeId: this.props.id })
+    const res = await searchCustomer({ name: name, size: this.state.size, from: this.state.from, representativeId: this.props.id })
     if (res.status === "success") {
       this.setState({
         totalCustomers: res.body.totalCount,
@@ -93,97 +92,92 @@ class CustomersForUser extends Component<Props, State> {
       Swal.fire("", `${local.doneMoving} (${this.state.selectedCustomers.length}) ${local.customerSuccess}`, "success").then(() => this.getCustomersForUser());
     } else {
       this.setState({ loading: false })
-      console.log(res);
     }
   }
   render() {
     return (
       <>
-              <div className="custom-card-header">
-              <Loader open={this.state.loading} type="fullsection" />
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <Card.Title style={{ marginLeft: 20, marginBottom: 0 }}>{local.customers}</Card.Title>
-                <span className="text-muted">{local.noOfCustomers + ` (${this.state.totalCustomers})`}</span>
-              </div>
-              <div>
-                <Can I='moveOfficerCustomers' a='user'><Button onClick={() => { this.setState({ openModal: true }) }} disabled={!Boolean(this.state.selectedCustomers.length)} className="big-button" style={{ marginLeft: 20 }}>{local.changeRepresentative} <span className="fa fa-exchange-alt"></span></Button></Can>
-              </div>
-            </div>
-      
-            <InputGroup style={{ direction: 'ltr', margin: "20px 0", }}>
-              <Form.Control
-                value={this.state.filterCustomers}
-                style={{ direction: 'rtl', borderRight: 0, padding: 22 }}
-                placeholder={local.searchByName}
-                onChange={async (e) => {console.log(e.currentTarget.value) ;this.setState({filterCustomers: e.currentTarget.value},()=>this.getCustomersForUser(this.state.filterCustomers) )} }
-              />
-              <InputGroup.Append>
-                <InputGroup.Text style={{ background: '#fff' }}><span className="fa fa-search fa-rotate-90"></span></InputGroup.Text>
-              </InputGroup.Append>
-            </InputGroup>
-            {this.state.totalCustomers > 0 ?
-            <Table striped hover style={{ textAlign: 'right' }}>
-              <thead>
-                <tr>
-                  <th><FormCheck type='checkbox' onClick={(e) => this.checkAll(e)}></FormCheck></th>
-                  <th>{local.customerCode}</th>
-                  <th>{local.customerName}</th>
-                  <th>{local.representative}</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.customers.map((customer, index) => {
-                    return (
-                      <tr key={index}>
-                        <td>
-                          <FormCheck type='checkbox'
-                            checked={this.state.selectedCustomers.includes(customer)}
-                            onChange={() => this.addRemoveItemFromChecked(customer)}>
-                          </FormCheck>
-                        </td>
-                        <td>{customer.code}</td>
-                        <td>{customer.customerName}</td>
-                        <td>{this.props.name}</td>
-                      </tr>
-                    )
-                  })}
-              </tbody>
-            </Table>
+        <div className="custom-card-header">
+          <Loader open={this.state.loading} type="fullsection" />
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Card.Title style={{ marginLeft: 20, marginBottom: 0 }}>{local.customers}</Card.Title>
+            <span className="text-muted">{local.noOfCustomers + ` (${this.state.totalCustomers})`}</span>
+          </div>
+          <div>
+            <Can I='moveOfficerCustomers' a='user'><Button onClick={() => { this.setState({ openModal: true }) }} disabled={!Boolean(this.state.selectedCustomers.length)} className="big-button" style={{ marginLeft: 20 }}>{local.changeRepresentative} <span className="fa fa-exchange-alt"></span></Button></Can>
+          </div>
+        </div>
+        <InputGroup style={{ direction: 'ltr', margin: "20px 0", }}>
+          <Form.Control
+            value={this.state.filterCustomers}
+            style={{ direction: 'rtl', borderRight: 0, padding: 22 }}
+            placeholder={local.searchByName}
+            onChange={async (e) => { this.setState({ filterCustomers: e.currentTarget.value }, () => this.getCustomersForUser(this.state.filterCustomers)) }}
+          />
+          <InputGroup.Append>
+            <InputGroup.Text style={{ background: '#fff' }}><span className="fa fa-search fa-rotate-90"></span></InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        {this.state.totalCustomers > 0 ?
+          <Table striped hover style={{ textAlign: 'right' }}>
+            <thead>
+              <tr>
+                <th><FormCheck type='checkbox' onClick={(e) => this.checkAll(e)}></FormCheck></th>
+                <th>{local.customerCode}</th>
+                <th>{local.customerName}</th>
+                <th>{local.representative}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.customers.map((customer, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <FormCheck type='checkbox'
+                        checked={this.state.selectedCustomers.includes(customer)}
+                        onChange={() => this.addRemoveItemFromChecked(customer)}>
+                      </FormCheck>
+                    </td>
+                    <td>{customer.code}</td>
+                    <td>{customer.customerName}</td>
+                    <td>{this.props.name}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
           :
           <div style={{ textAlign: 'center', marginBottom: 40 }}>
             <img alt='no-data-found' src={require('../../Assets/no-results-found.svg')} />
             <h4>{local.noResultsFound}</h4>
           </div>
         }
-            <Modal size="lg" show={this.state.openModal} centered onHide={() => this.setState({ openModal: false })}>
-              <Modal.Header closeButton>
-                <Modal.Title>{local.chooseRepresentative}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <Row style={{ padding: '10px 40px' }}>
-                  <Col sm={9}>
-                    <LoanOfficersDropDown onSelectLoanOfficer={(LO) => this.setState({ selectedLO: LO })} excludeId={this.props.id} />
-                  </Col>
-                  <Col sm={3}>
-                    <Button style={{ width: '100%', height: '100%' }}
-                      onClick={() => this.submit()}
-                      disabled={!Boolean(this.state.selectedLO._id)}
-                      variant="primary">{local.submit}</Button>
-                  </Col>
-                </Row>
-              </Modal.Body>
-            </Modal>
-            <Pagination 
-             totalCount = {this.state.totalCustomers}
-             pagination = {true}
-             dataLength = {this.state.customers.length}
-
-             changeNumber={(key: string, number: number) => {
-              this.setState({ [key]: number } as any, () => this.getCustomersForUser());
-            }}
-            />
-       
-
+        <Modal size="lg" show={this.state.openModal} centered onHide={() => this.setState({ openModal: false })}>
+          <Modal.Header closeButton>
+            <Modal.Title>{local.chooseRepresentative}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Row style={{ padding: '10px 40px' }}>
+              <Col sm={9}>
+                <LoanOfficersDropDown onSelectLoanOfficer={(LO) => this.setState({ selectedLO: LO })} excludeId={this.props.id} />
+              </Col>
+              <Col sm={3}>
+                <Button style={{ width: '100%', height: '100%' }}
+                  onClick={() => this.submit()}
+                  disabled={!Boolean(this.state.selectedLO._id)}
+                  variant="primary">{local.submit}</Button>
+              </Col>
+            </Row>
+          </Modal.Body>
+        </Modal>
+        <Pagination
+          totalCount={this.state.totalCustomers}
+          pagination={true}
+          dataLength={this.state.customers.length}
+          changeNumber={(key: string, number: number) => {
+            this.setState({ [key]: number } as any, () => this.getCustomersForUser());
+          }}
+        />
       </>
     )
   }
