32b98f88698f47d520d7743ed892198e867bc40f
Ahmed Fahmy <afahmy31@gmail.com>
2020-07-20 14:28:18 +0200

    finish documentsUploadedPercentage pdf

---
+++ src/Mohassel/Components/pdfTemplates/documentsUploadedPercentage/documentsUploadedPercentage.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './documentsUploadedPercentage.scss';
+
+const DocumentsUploadedPercentage = (props) => {
+    const bigArr: any = [];
+    for (let index = 0; index < 200; index++) {
+        bigArr.push({ name: 'cairo-shobra', number1: 25, number2: 25, percentage: '100%' })
+    }
+    const parentArr: any = [];
+    const splitNUmber = 50;
+    let start = 0;
+    while (start < bigArr.length) {
+        console.log("slice")
+        parentArr.push(bigArr.slice(start, start + splitNUmber));
+        start = start + 50;
+    }
+    return (
+        <div className="documents-uploaded-percentage" lang="ar">
+            <Row>
+                {parentArr.map((arr, index) => {
+                    return (
+                        <Col sm={6}>
+                            <table key={index}>
+                                <thead>
+                                    <tr>
+                                        <th className="frame" colSpan={100}>نسب المسح الضوئى عن الفترة من 26-6-2020 2 الي 25-6-2020</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {arr.map((el, key) => {
+                                        return (
+                                            <tr key={key}>
+                                                <td className="frame" colSpan={1}>{(key + 1) + (index * 50)}</td>
+                                                <td className="frame" colSpan={4}>{el.name}</td>
+                                                <td className="frame" colSpan={4}>{el.number1}</td>
+                                                <td className="frame" colSpan={4}>{el.number2}</td>
+                                                <td className="frame yellow" colSpan={4}>{el.percentage}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </Col>
+                    )
+                })}
+            </Row>
+        </div>
+    )
+}
+
+export default DocumentsUploadedPercentage;
