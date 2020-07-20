ff83d7d46a2962f2150421b40427503d038e17d1
Sara Alaa <sara3laa.93@gmail.com>
2020-07-20 14:21:13 +0200

    actionLogs - skeleton

---
+++ src/Mohassel/Components/ActionLogs/action-logs.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class ActionLogs extends Component {
+    render() {
+        return (
+            <div>
+                ActionLogs
+            </div>
+        )
+    }
+}
+
+export default ActionLogs
