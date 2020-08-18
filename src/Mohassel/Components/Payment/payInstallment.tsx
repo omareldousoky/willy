30cad962202fc2b0cd425312c062a1e3a557add7
Ahmed Fahmy <afahmy31@gmail.com>
2020-08-18 15:16:39 +0200

    fix required amount

--- src/Mohassel/Components/Payment/payInstallment.tsx
+++ src/Mohassel/Components/Payment/payInstallment.tsx
@@ -21,6 +21,7 @@ interface FormValues {
   paymentType: string;
   penaltyAction: string;
   byInsurance: boolean;
+  installmentNumber: number;
 }
 interface Installment {
   id: number;
@@ -65,6 +66,7 @@ interface State {
   paymentType: string;
   randomPaymentTypes: Array<SelectObject>;
   penaltyAction: string;
+  installmentNumber: number;
 }
 class PayInstallment extends Component<Props, State> {
   constructor(props: Props) {
@@ -83,7 +85,8 @@ class PayInstallment extends Component<Props, State> {
         { label: local.toktokStamp, value: "toktokStamp" },
         { label: local.tricycleStamp, value: "tricycleStamp" }
       ],
-      penaltyAction: this.props.penaltyAction
+      penaltyAction: this.props.penaltyAction,
+      installmentNumber: -1,
     };
   }
   componentDidUpdate(prevProps, prevState) {
@@ -92,19 +95,10 @@ class PayInstallment extends Component<Props, State> {
   }
 
   getRequiredAmount() {
-    // const todaysDate = new Date("2020-06-30").valueOf();
-    const todaysDate = new Date().valueOf();
-    let total = 0;
-    const installments: Array<number> = [];
-    this.props.installments.forEach(installment => {
-      if (todaysDate >= installment.dateOfPayment) {
-        if (installment.status !== "paid")
-          total =
-            total + installment.installmentResponse - installment.totalPaid;
-        installments.push(installment.id);
-      } else return total;
+    const installment = this.props.installments.find(installment => {
+        return (installment.status !== "paid" && installment.status !== "rescheduled" && installment.status !== "pending")
     });
-    return { total: total, installments: installments };
+    return installment? installment.installmentResponse - installment.totalPaid : 0;
   }
   render() {
     return (
@@ -207,7 +201,7 @@ class PayInstallment extends Component<Props, State> {
                               name="requiredAmount"
                               value={
                                 formikBag.values.requiredAmount ||
-                                this.getRequiredAmount().total
+                                this.getRequiredAmount()
                               }
                               disabled
                             ></Form.Control>
