c803aa7fb71e4395227d0ee4855fc5ced91c6188
Ahmed Fahmy <afahmy31@gmail.com>
2020-08-19 13:15:06 +0200

    fix build error

--- src/Mohassel/Services/APIs/createIssueLoan/issueLoan.ts
+++ src/Mohassel/Services/APIs/createIssueLoan/issueLoan.ts
@@ -1,7 +1,7 @@
 import axios from '../axios-instance';
 interface LoanIssuanceObj {
     id: string;
-    loanIssuanceDate:number;
+    loanIssuanceDate: number;
     fieldManagerId?: string;
     managerVisitDate?: number;
 }
