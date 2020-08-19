9e789009b072254fb6905e2904a9c9f813a2af80
Ahmed Fahmy <afahmy31@gmail.com>
2020-08-19 13:05:48 +0200

    add type of obj of loan issuance api

--- src/Mohassel/Services/APIs/createIssueLoan/issueLoan.ts
+++ src/Mohassel/Services/APIs/createIssueLoan/issueLoan.ts
@@ -1,6 +1,11 @@
 import axios from '../axios-instance';
-
-export const issueLoan = async (obj) => {
+interface LoanIssuanceObj {
+    id: string;
+    loanIssuanceDate:number;
+    fieldManagerId?: string;
+    managerVisitDate?: number;
+}
+export const issueLoan = async (obj: LoanIssuanceObj) => {
     const url = process.env.REACT_APP_BASE_URL + `/application/issue/${obj.id}`;
     try {
         const res = await axios.put(url, obj);
