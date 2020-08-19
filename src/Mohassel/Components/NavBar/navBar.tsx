2c3a00f249fb9d30671b8560b133cf176c7f7caf
Sara Alaa <sara3laa.93@gmail.com>
2020-08-19 18:33:58 +0200

    add getCustomer permission

--- src/Mohassel/Components/NavBar/navBar.tsx
+++ src/Mohassel/Components/NavBar/navBar.tsx
@@ -178,7 +178,7 @@ class NavBar extends Component<Props, State> {
               {<Can I='documentTypes' a='config'><Nav.Link onClick={() => this.props.history.push('/tools/encoding-files')}>{local.tools}</Nav.Link> </Can>}
               {<Can I='getIssuedLoan' a='application'><Nav.Link onClick={() => this.props.history.push('/loans')}>{local.issuedLoans}</Nav.Link></Can>}
             {<Can  I="viewActionLogs" a='user' ><Nav.Link onClick={()=> this.props.history.push('/logs')}>{local.logs}</Nav.Link></Can>}
-            {<Can I = "changeOfficer" a  ="customer"><Nav.Link onClick={()=> this.props.history.push('/move-customers')}>{local.moveCustomers}</Nav.Link></Can>}
+            {<Can I = "changeOfficer" a  ="customer"><Can  I='getCustomer' a='customer'><Nav.Link onClick={()=> this.props.history.push('/move-customers')}>{local.moveCustomers}</Nav.Link></Can></Can>}
             </Nav>
           </Navbar.Collapse>
         </Navbar>}
