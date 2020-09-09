5a307a98a8e90a5f958ae5be7c5c8ebf7afbae97
Ahmed Fahmy <afahmy31@gmail.com>
2020-09-09 17:10:22 +0200

    fix TPAY file and add Trailing lines to all cib files

--- src/Mohassel/Components/CIB/textFiles.ts
+++ src/Mohassel/Components/CIB/textFiles.ts
@@ -9,7 +9,7 @@ const getYearMonthDay = (dateTimeStamp: number) => {
     }
     const year = date.getFullYear();
     const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
-    const day = date.getDate() + 1 < 10 ? `0${date.getDate()}` : date.getDate();
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
     return `${year}${month}${day}`
 }
 const getGender = (gender: string) => {
@@ -48,6 +48,12 @@ const numTo2Decimal = (num: number | string) => {
     return (Math.round(num * 100) / 100).toFixed(2);
 }
 
+const sameDay = (paidAt: number, dateOfPay: number) => {
+    const paidAtD = new Date(paidAt);
+    const dateOfPayD = new Date(dateOfPay);
+    return ((paidAtD.getFullYear() === dateOfPayD.getFullYear()) && (paidAtD.getMonth() === dateOfPayD.getMonth()) && (paidAtD.getDate() === dateOfPayD.getDate()))
+}
+
 const cusTxt = (textData) => {
     return (`H|${getYearMonthDay(0)}|${textData.length}|TDIS_CUS|\n` +
         textData.map(application => {
@@ -55,7 +61,7 @@ const cusTxt = (textData) => {
             return (
                 `${getYearMonthDay(customer.created.at)}|${customer.key}    |N|D|${customer.customerName}|SINGLE|${getGender(customer.gender)}|${getYearMonthDay(customer.birthDate)}|${customer.nationalId}|National ID|EG||${customer.customerHomeAddress}|${getYearMonthDay(application.created.at)}|990|1| M5| 097|199|2|516|4100|||EG|||${customer.customerName}|${customer.customerHomeAddress}|29|15|9|1|5|1|other||\n`
             )
-        })).split(',').join('')
+        }) + `T|${getYearMonthDay(0)}|${textData.length}|TDIS_CUS|\n`).split(',').join('')
 }
 
 const finText = (textData) => {
@@ -65,7 +71,7 @@ const finText = (textData) => {
             return (
                 `D|${customer.key}          |N|${getYearMonthDay(application.issueDate)}|${application.principal ? numTo2Decimal(application.principal) : numTo2Decimal(0)}|${application.product.currency.toUpperCase()}|D|0||${application.product.interest ? (application.product.interest + ".0000000") : '0.0000000'}|${getYearMonthDay(application.installmentsObject.output[application.installmentsObject.output.length - 1].dateOfPayment)}|${application.installmentsObject.output.length}|${application.installmentsObject.output[0].principalInstallment ? numTo2Decimal(application.installmentsObject.output[0].principalInstallment) : numTo2Decimal(0)}|${application.installmentsObject.output[0].feesInstallment ? numTo2Decimal(application.installmentsObject.output[0].feesInstallment) : numTo2Decimal(0)}|${periodType(application.product.periodType)}|${getYearMonthDay(application.installmentsObject.output[0].dateOfPayment)}|${application.loanApplicationKey}      |${getBeneficiaryType(application.product.beneficiaryType)}|\n`
             )
-        })).split(',').join('')
+        }) + `T|${getYearMonthDay(0)}|${getTotalPrincipals(textData)}|${textData.length}|TDIS_FIN|\n`).split(',').join('')
 }
 
 const instText = (textData) => {
@@ -76,10 +82,11 @@ const instText = (textData) => {
                     `D|${application.loanApplicationKey}      |${installment.id? installment.id: 0}|${application.installmentsObject.output.length}|${getYearMonthDay(installment.dateOfPayment)}|${installment.principalInstallment ? numTo2Decimal(installment.principalInstallment) : numTo2Decimal(0)}|${installment.feesInstallment ? numTo2Decimal(installment.feesInstallment) : numTo2Decimal(0)}|${installment.installmentResponse ? numTo2Decimal(installment.installmentResponse) : numTo2Decimal(0)}|\n`
                 )
             })
-        })).split(',').join('')
+        }) + `T|${getYearMonthDay(0)}||${getTotalNumberOfLines(textData)}|TDIS_INST|\n`).split(',').join('')
 }
 
-const payText = (textData) => {
+const payText = (textData, dateOfPay: number) => {
+    console.log('DATE OF PAY', dateOfPay)
     let totalPrincipal = 0;
     let totalInterest = 0;
     let totalNoOfInstallments = 0;
@@ -92,27 +99,28 @@ const payText = (textData) => {
             }
         })
     });
-    return (`H|${getYearMonthDay(0)}|${numTo2Decimal(totalPrincipal)}|${numTo2Decimal(totalInterest)}|${totalNoOfInstallments * 3}|TPAY|\n` +
+    return (`H|${getYearMonthDay(dateOfPay)}|${numTo2Decimal(totalPrincipal)}|${numTo2Decimal(totalInterest)}|${totalNoOfInstallments * 3}|TPAY|\n` +
         textData.map(application => {
             const customer = application.product.beneficiaryType === "group" ? application.group.individualsInGroup.find((member) => member.type === "leader").customer : application.customer;
             let final = '';
             application.installmentsObject.output?.map((installment, index) => {
-                if (installment.status === "paid") {
-                    final = final + `D|${customer.key}      |I|${getYearMonthDay(application.issueDate)}|${installment.principalInstallment ? numTo2Decimal(installment.principalInstallment) : numTo2Decimal(0)}|EGP|C|0||||${installment.id ? installment.id : 0}||||${getYearMonthDay(application.installmentsObject.output[(index + 1) > application.installmentsObject.output.length - 1 ? index : index + 1].dateOfPayment)}|${application.loanApplicationKey}    |${getBeneficiaryType(application.product.beneficiaryType)}|\n`
+                if (installment.status === "paid" && sameDay(installment.paidAt, dateOfPay)) {
+                    final = final + `D|${customer.key}      |I|${getYearMonthDay(dateOfPay)}|${installment.principalInstallment ? numTo2Decimal(installment.principalInstallment) : numTo2Decimal(0)}|EGP|C|0||||${installment.id ? installment.id : 0}||||${getYearMonthDay(application.installmentsObject.output[(index + 1) > application.installmentsObject.output.length - 1 ? index : index + 1].dateOfPayment)}|${application.loanApplicationKey}    |${getBeneficiaryType(application.product.beneficiaryType)}|\n`
                 }
             })
             application.installmentsObject.output?.map((installment, index) => {
-                if (installment.status === "paid") {
-                    final = final + `D|${customer.key}      |T|${getYearMonthDay(application.issueDate)}|${installment.feesInstallment ? numTo2Decimal((Number(installment.feesInstallment) * 0.40)) : numTo2Decimal(0)}|EGP|C|0||||${installment.id ? installment.id : 0}||||${getYearMonthDay(application.installmentsObject.output[(index + 1) > application.installmentsObject.output.length - 1 ? index : index + 1].dateOfPayment)}|${application.loanApplicationKey}    |${getBeneficiaryType(application.product.beneficiaryType)}|\n`
+                if (installment.status === "paid" && sameDay(installment.paidAt, dateOfPay)) {
+                    final = final + `D|${customer.key}      |T|${getYearMonthDay(dateOfPay)}|${installment.feesInstallment ? numTo2Decimal((Number(installment.feesInstallment) * 0.40)) : numTo2Decimal(0)}|EGP|C|0||||${installment.id ? installment.id : 0}||||${getYearMonthDay(application.installmentsObject.output[(index + 1) > application.installmentsObject.output.length - 1 ? index : index + 1].dateOfPayment)}|${application.loanApplicationKey}    |${getBeneficiaryType(application.product.beneficiaryType)}|\n`
                 }
             })
             application.installmentsObject.output?.map((installment, index) => {
-                if (installment.status === "paid") {
-                    final = final + `D|${customer.key}      |R|${getYearMonthDay(application.issueDate)}|${installment.feesInstallment ? numTo2Decimal((Number(installment.feesInstallment) * 0.60)) : numTo2Decimal(0)}|EGP|C|0||||${installment.id ? installment.id : 0}||||${getYearMonthDay(application.installmentsObject.output[(index + 1) > application.installmentsObject.output.length - 1 ? index : index + 1].dateOfPayment)}|${application.loanApplicationKey}    |${getBeneficiaryType(application.product.beneficiaryType)}|\n`
+                if (installment.status === "paid" && sameDay(installment.paidAt, dateOfPay)) {
+                    final = final + `D|${customer.key}      |R|${getYearMonthDay(dateOfPay)}|${installment.feesInstallment ? numTo2Decimal((Number(installment.feesInstallment) * 0.60)) : numTo2Decimal(0)}|EGP|C|0||||${installment.id ? installment.id : 0}||||${getYearMonthDay(application.installmentsObject.output[(index + 1) > application.installmentsObject.output.length - 1 ? index : index + 1].dateOfPayment)}|${application.loanApplicationKey}    |${getBeneficiaryType(application.product.beneficiaryType)}|\n`
                 }
             })
             return final;
-        })).split(',').join('')
+        }) + `T|${getYearMonthDay(dateOfPay)}|${numTo2Decimal(totalPrincipal)}|${numTo2Decimal(totalInterest)}|${totalNoOfInstallments * 3}|TPAY|`
+        ).split(',').join('')
 }
 
 const trfText = (textData) => {
@@ -133,11 +141,11 @@ const trfText = (textData) => {
 }
 
 
-export const downloadTxtFile = (textData, tPay: boolean) => {
+export const downloadTxtFile = (textData, tPay: boolean, dateOfPay: number) => {
     let filesArr: Array<TextReport> = [];
     if (tPay) {
         filesArr = [
-            { name: 'TPAY', func: (textData) => payText(textData) },
+            { name: 'TPAY', func: (textData) => payText(textData, dateOfPay) },
         ];
     } else {
         filesArr = [
