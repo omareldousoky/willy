626467c9ac3363df9e658889f40d8f642bd5bdf5
Sara Alaa <sara3laa.93@gmail.com>
2020-09-16 14:18:14 +0200

    update url

--- src/Mohassel/Services/APIs/User-Creation/checkHRCodeDUP.ts
+++ src/Mohassel/Services/APIs/User-Creation/checkHRCodeDUP.ts
@@ -1,7 +1,7 @@
 import axios from '../axios-instance';
 
 export const checkHRCodeDuplicates = async (hrCode: string) => {
-    const url = process.env.REACT_APP_BASE_URL + `/user/checkHRCode?hrCode=${hrCode}`;
+    const url = process.env.REACT_APP_BASE_URL + `/user/check-hr-code?hrCode=${hrCode}`;
     try {
         const res = await axios.get(url);
         return { status: "success", body: res.data }
