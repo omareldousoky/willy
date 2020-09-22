fad8b7d9ab6c18c7e2fdeeaff42d99d9f5d1c496
Omar Eldousoky <omareldousoky@gmail.com>
2020-09-22 17:19:51 +0200

    done with iScore

--- src/Mohassel/Components/Reports/iscoreReports.tsx
+++ src/Mohassel/Components/Reports/iscoreReports.tsx
@@ -4,6 +4,7 @@ import { Loader } from '../../../Shared/Components/Loader';
 import * as local from '../../../Shared/Assets/ar.json';
 import Button from 'react-bootstrap/Button';
 import { getiScoreReportRequests, generateiScoreReport } from '../../Services/APIs/Reports/iScoreReports';
+import { downloadFile, getIscoreReportStatus, timeToDateyyymmdd, timeToArabicDate } from '../../Services/utils';
 
 interface State {
     showModal?: boolean;
@@ -19,33 +20,33 @@ class IscoreReports extends Component<{}, State>{
             data: []
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getiScoreReports()
     }
-    async getiScoreReports(){
+    async getiScoreReports() {
         this.setState({ loading: true })
         const res = await getiScoreReportRequests();
         if (res.status === 'success') {
             this.setState({
-              data: res.body,
-              showModal: false,
-              loading: false,
+                data: res.body.data,
+                showModal: false,
+                loading: false,
             })
         } else {
-          this.setState({ loading: false });
-          console.log(res)
+            this.setState({ loading: false });
+            console.log(res)
         }
     }
-    async generateReport(){
+    async generateReport() {
         this.setState({ loading: true })
         const res = await generateiScoreReport();
         if (res.status === 'success') {
             this.setState({
-              loading: false,
+                loading: false,
             })
         } else {
-          this.setState({ loading: false });
-          console.log(res)
+            this.setState({ loading: false });
+            console.log(res)
         }
     }
     render() {
@@ -56,17 +57,21 @@ class IscoreReports extends Component<{}, State>{
                     <Card.Body style={{ padding: 15 }}>
                         <div className="custom-card-header">
                             <Card.Title style={{ marginLeft: 20, marginBottom: 0 }}>{local.iScoreReports}</Card.Title>
-                            <Button type='button' variant='primary' onClick={() => this.generateReport()}>Request</Button>
+                            <Button type='button' variant='primary' onClick={() => this.generateReport()}>{local.requestNewreport}</Button>
                         </div>
                         {this.state.data.length > 0 ? this.state.data.map((pdf, index) => {
                             return (
                                 <Card key={index}>
                                     <Card.Body>
                                         <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0px 20px', fontWeight: 'bold', alignItems: 'center' }}>
-                                            <div>
+                                            <div style={{ display: 'flex' }}>
                                                 <span style={{ marginLeft: 40 }}>#{index + 1}</span>
+                                                <span style={{ marginLeft: 40 }}>{pdf.fileName}</span>
+                                                <span style={{ marginLeft: 40, display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}><span>{local.loanAppCreationDate}</span>{timeToArabicDate(pdf.requestedAt, true)}</span>
+                                                <span style={{ marginLeft: 40 }}>{getIscoreReportStatus(pdf.status)}</span>
+                                                {pdf.status === 'created' && <span style={{ marginLeft: 40, display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}><span>{local.creationDate}</span>{timeToArabicDate(pdf.fileCreatedAt, true)}</span>}
                                             </div>
-                                            <img style={{ cursor: 'pointer' }} alt="download" data-qc="download" src={require(`../../Assets/green-download.svg`)} onClick={() => console.log(pdf)} />
+                                            {pdf.status === 'created' && pdf.url && <img style={{ cursor: 'pointer' }} alt="download" data-qc="download" src={require(`../../Assets/green-download.svg`)} onClick={() => downloadFile(pdf.url)} />}
                                         </div>
                                     </Card.Body>
                                 </Card>
