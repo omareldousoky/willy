02e060e350a025279ecefda5cde04922601e5876
Omar Eldousoky <omareldousoky@gmail.com>
2020-09-23 18:31:23 +0200

    done with salems changes in apis

--- src/Mohassel/Components/Reports/iscoreReports.tsx
+++ src/Mohassel/Components/Reports/iscoreReports.tsx
@@ -3,8 +3,9 @@ import Card from 'react-bootstrap/Card';
 import { Loader } from '../../../Shared/Components/Loader';
 import * as local from '../../../Shared/Assets/ar.json';
 import Button from 'react-bootstrap/Button';
-import { getiScoreReportRequests, generateiScoreReport } from '../../Services/APIs/Reports/iScoreReports';
-import { downloadFile, getIscoreReportStatus, timeToDateyyymmdd, timeToArabicDate } from '../../Services/utils';
+import { getiScoreReportRequests, generateiScoreReport, getiScoreReport } from '../../Services/APIs/Reports/iScoreReports';
+import { downloadFile, getIscoreReportStatus, timeToArabicDate } from '../../Services/utils';
+import Swal from 'sweetalert2';
 
 interface State {
     data: any;
@@ -31,6 +32,7 @@ class IscoreReports extends Component<{}, State>{
             })
         } else {
             this.setState({ loading: false });
+            Swal.fire("error", local.searchError, 'error')
             console.log(res)
         }
     }
@@ -38,11 +40,25 @@ class IscoreReports extends Component<{}, State>{
         this.setState({ loading: true })
         const res = await generateiScoreReport();
         if (res.status === 'success') {
+            Swal.fire("success", local.fileQueuedSuccess, 'success')
             this.setState({
                 loading: false,
             },()=>{this.getiScoreReports()})
         } else {
             this.setState({ loading: false });
+            Swal.fire("error", local.fileQueuedError, 'error')
+            console.log(res)
+        }
+    }
+    async getFile(fileRequest){
+        const res = await getiScoreReport(fileRequest._id);
+        if (res.status === 'success') {
+            this.setState({
+                loading: false,
+            },()=>{ downloadFile(res.body.presignedUrl) })
+        } else {
+            this.setState({ loading: false });
+            Swal.fire("error", local.searchError, 'error')
             console.log(res)
         }
     }
@@ -63,12 +79,12 @@ class IscoreReports extends Component<{}, State>{
                                         <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0px 20px', fontWeight: 'bold', alignItems: 'center' }}>
                                             <div style={{ display: 'flex' }}>
                                                 <span style={{ marginLeft: 40 }}>#{index + 1}</span>
-                                                <span style={{ marginLeft: 40 }}>{pdf.fileName}</span>
                                                 <span style={{ marginLeft: 40, display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}><span>{local.loanAppCreationDate}</span>{timeToArabicDate(pdf.created.at, true)}</span>
                                                 <span style={{ marginLeft: 40 }}>{getIscoreReportStatus(pdf.status)}</span>
-                                                {pdf.status === 'created' && <span style={{ marginLeft: 40, display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}><span>{local.creationDate}</span>{timeToArabicDate(pdf.fileCreatedAt, true)}</span>}
+                                                <span style={{ marginLeft: 40 }}>{pdf.fileName}</span>
+                                                {pdf.status === 'created' && <span style={{ marginLeft: 40, display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}><span>{local.creationDate}</span>{timeToArabicDate(pdf.fileGeneratedAt, true)}</span>}
                                             </div>
-                                            {pdf.status === 'created' && pdf.url && <img style={{ cursor: 'pointer' }} alt="download" data-qc="download" src={require(`../../Assets/green-download.svg`)} onClick={() => downloadFile(pdf.url)} />}
+                                            {pdf.status === 'created' && <img style={{ cursor: 'pointer' }} alt="download" data-qc="download" src={require(`../../Assets/green-download.svg`)} onClick={() => this.getFile(pdf)} />}
                                         </div>
                                     </Card.Body>
                                 </Card>
--- src/Mohassel/Services/APIs/Reports/iScoreReports.ts
+++ src/Mohassel/Services/APIs/Reports/iScoreReports.ts
@@ -20,3 +20,13 @@ export const generateiScoreReport = async () => {
         return { status: "error", error: error.response.data }
     }
 }
+export const getiScoreReport = async (id: string) => {
+    const url = process.env.REACT_APP_BASE_URL + `/report/download-iscore-file/${id}`;
+    try {
+        const res = await axios.get(url);
+        return { status: "success", body: res.data }
+    }
+    catch (error) {
+        return { status: "error", error: error.response.data }
+    }
+}
