ac462b1849981969940a9b2cb16ddbbc94ea3d48
Ahmed Fahmy <afahmy31@gmail.com>
2020-11-02 13:49:02 +0200

    no need for the change loan officer file for now

--- src/Mohassel/Components/HalanIntegration/leads.tsx
+++ src/Mohassel/Components/HalanIntegration/leads.tsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Card } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import AsyncSelect from 'react-select/async';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Swal from 'sweetalert2';
@@ -9,6 +15,8 @@ import DynamicTable from '../DynamicTable/dynamicTable';
 import Search from '../Search/search';
 import { getDateAndTime } from '../../Services/getRenderDate';
 import { changeLeadState, changeInReviewLeadState } from '../../Services/APIs/Leads/changeLeadState';
+import { searchLoanOfficer } from '../../Services/APIs/LoanOfficers/searchLoanOfficer';
+import { assignLeadToLO } from '../../Services/APIs/Leads/assignLeadToLO';
 import { search } from '../../redux/search/actions';
 import { loading } from '../../redux/loading/actions';
 import local from '../../../Shared/Assets/ar.json';
@@ -34,6 +42,10 @@ interface State {
   size: number;
   from: number;
   openActionsId: string;
+  openModal: boolean;
+  loanOfficers: Array<any>;
+  selectedLO: any;
+  selectedLead: any;
 }
 class Leads extends Component<Props, State>{
   mappers: { title: string; key: string; sortable?: boolean; render: (data: any) => void }[]
@@ -47,16 +59,14 @@ class Leads extends Component<Props, State>{
           desc: local.createAndEditApplicantLeads,
           path: '/halan-integration/leads',
         },
-        {
-          icon: 'exchange',
-          header: local.changeRepresentative,
-          desc: local.changeOfficerForMoreThanOneCustomer,
-          path: '/halan-integration/exchange',
-        }
       ],
       size: 10,
       from: 0,
       openActionsId: '',
+      openModal: false,
+      loanOfficers: [],
+      selectedLO: {},
+      selectedLead: {}
     }
     this.mappers = [
       {
@@ -103,17 +113,22 @@ class Leads extends Component<Props, State>{
         render: data => data.createdAt ? getDateAndTime(data.createdAt) : ''
       },
       {
+        title: local.loanOfficer,
+        key: "loanOfficer",
+        render: data => <div>{data.loanOfficerName} <span style={{ marginRight: 5, cursor: 'pointer' }} className="fa fa-exchange-alt" onClick={() => this.setState({ selectedLead: data, openModal: true })} /></div>
+      },
+      {
         title: local.actions,
         key: "actions",
         render: data =>
           <div style={{ position: 'relative' }}>
             <p className="clickable-action" onClick={() => this.setState({ openActionsId: this.state.openActionsId === data.uuid ? '' : data.uuid })}>{local.actions}</p>
             {this.state.openActionsId === data.uuid && <div className="actions-list">
-              {data.status === "in-review" && <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, 'rejected', '')}>{local.rejectApplication}</div>}
-              {data.status === "in-review" && <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, 'approved', '')}>{local.acceptApplication}</div>}
-              <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, 'in-review', 'secondApproval')}>{local.acceptSecondVisit}</div>
-              <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, 'in-review', 'basic')}>{local.editLead}</div>
-              <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, 'in-review', 'basic')}>{local.viewCustomerLead}</div>
+              {data.status === "in-review" && <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, data.status, data.inReviewStatus, 'rejected', '')}>{local.rejectApplication}</div>}
+              {data.status === "in-review" && <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, data.status, data.inReviewStatus, 'approved', '')}>{local.acceptApplication}</div>}
+              {data.inReviewStatus !== "secondApproval" && data.status !== "approved" && data.status !== "rejected" && <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, data.status, data.inReviewStatus, 'in-review', 'secondApproval')}>{local.acceptSecondVisit}</div>}
+              <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, data.status, data.inReviewStatus, 'in-review', 'basic')}>{local.editLead}</div>
+              <div className="item" onClick={() => this.changeLeadState(data.phoneNumber, data.status, data.inReviewStatus, 'in-review', 'basic')}>{local.viewCustomerLead}</div>
             </div>}
           </div>
       },
@@ -126,25 +141,52 @@ class Leads extends Component<Props, State>{
   getLeadsCustomers() {
     this.props.search({ ...this.props.searchFilters, size: this.state.size, from: this.state.from, url: 'lead' });
   }
-  async changeLeadState(phoneNumber: string, newState: string, inReviewStatus: string) {
+  async changeLeadState(phoneNumber: string, oldState: string, oldInReviewStatus: string, newState: string, inReviewStatus: string) {
     this.props.setLoading(true);
-    const res = await changeLeadState(phoneNumber, newState);
-    if (res.status === "success") {
-      this.props.setLoading(false);
-      this.setState({ openActionsId: "" })
-      if(inReviewStatus !== '') {
+    if (oldState === newState) {
+      if (oldInReviewStatus === 'basic') {
         const inReviewStatusRes = await changeInReviewLeadState(phoneNumber, inReviewStatus);
-        if(inReviewStatusRes.status === "success") {
+        if (inReviewStatusRes.status === "success") {
+          this.props.setLoading(false);
+          this.setState({ openActionsId: "" })
           Swal.fire('', local.changeState, 'success').then(() => this.getLeadsCustomers());
         } else {
           this.props.setLoading(false);
           Swal.fire('', local.userRoleEditError, 'error');
         }
       }
-      Swal.fire('', local.changeState, 'success').then(() => this.getLeadsCustomers());
+    } else {
+      const res = await changeLeadState(phoneNumber, newState);
+      if (res.status === "success") {
+        this.props.setLoading(false);
+        this.setState({ openActionsId: "" })
+        Swal.fire('', local.changeState, 'success').then(() => this.getLeadsCustomers());
+      } else {
+        this.props.setLoading(false);
+        Swal.fire('', local.userRoleEditError, 'error');
+      }
+    }
+  }
+  getLoanOfficers = async (input: string) => {
+    const res = await searchLoanOfficer({ from: 0, size: 1000, name: input });
+    if (res.status === "success") {
+      this.setState({ loanOfficers: res.body.data })
+      return res.body.data.filter(loanOfficer => loanOfficer.branches.includes(this.state.selectedLead.branchId));
+    } else {
+      this.setState({ loanOfficers: [] })
+      return [];
+    }
+  }
+  async submit() {
+    this.props.setLoading(true);
+    const res = await assignLeadToLO(this.state.selectedLead.phoneNumber, this.state.selectedLO._id, this.state.selectedLead.uuid);
+    if (res.status === "success") {
+      this.props.setLoading(false);
+      this.setState({ openModal: false })
+      Swal.fire("", `${local.doneMoving} ${local.customerSuccess}`, "success").then(() => this.setState({selectedLO: {}, selectedLead: {}}))
     } else {
       this.props.setLoading(false);
-      Swal.fire('', local.userRoleEditError, 'error');
+      Swal.fire("", local.errorOnMovingCustomers, "error")
     }
   }
   render() {
@@ -191,6 +233,42 @@ class Leads extends Component<Props, State>{
             }
           </Card.Body>
         </Card>
+        <Modal size="lg" show={this.state.openModal} onHide={() => this.setState({ openModal: false })}>
+          <Modal.Header closeButton>
+            <Modal.Title style={{ margin: " 0 auto" }}>
+              {local.chooseRepresentative}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Row style={{ padding: "10px 40px" }}>
+              <Form.Label className="data-label">{local.chooseLoanOfficer}</Form.Label>
+              <Col sm={12}>
+                <AsyncSelect
+                  name="employees"
+                  data-qc="employees"
+                  value={this.state.loanOfficers.find(loanOfficer => loanOfficer._id === this.state.selectedLO?._id)}
+                  onChange={(loanOfficer) => this.setState({ selectedLO: loanOfficer })}
+                  getOptionLabel={(option) => option.name}
+                  getOptionValue={(option) => option._id}
+                  loadOptions={(input) => this.getLoanOfficers(input)}
+                  cacheOptions defaultOptions
+                />
+              </Col>
+            </Row>
+            <Row style={{ padding: "10px 40px", justifyContent: "center" }}>
+              <Col >
+                <Button
+                  style={{ width: "100%", height: "100%" }}
+                  onClick={() => this.submit()}
+                  disabled={false}
+                  variant="primary"
+                >
+                  {local.submit}
+                </Button>
+              </Col>
+            </Row>
+          </Modal.Body>
+        </Modal>
       </>
     )
   }
