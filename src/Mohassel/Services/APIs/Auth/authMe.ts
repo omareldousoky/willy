aa67ee69a4d098786b69c966628e2a59b2bf8c9f
Ahmed Fahmy <afahmy31@gmail.com>
2020-11-03 13:04:03 +0200

    change env variable of ltsbranch

--- src/Mohassel/Services/APIs/Auth/authMe.ts
+++ src/Mohassel/Services/APIs/Auth/authMe.ts
@@ -5,7 +5,7 @@ export const authMe = async () => {
     try {
         const res = await axios.get(url);
         if (res.data.validBranches && res.data.validBranches.length > 0) {
-            document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_LTS_SUBDOMAIN ? `;domain=${process.env.REACT_APP_LTS_SUBDOMAIN}` : '' + ';path=/;');
+            document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_DOMAIN ? `;domain=${process.env.REACT_APP_DOMAIN}` : '' + ';path=/;');
         }
         return { status: "success", body: res.data }
     }
