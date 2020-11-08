cebd2b9a136b93debd3ce102b904955e86445de8
Ahmed Fahmy <afahmy31@gmail.com>
2020-11-08 17:09:09 +0200

    fix referesh cookies

--- src/Mohassel/Services/APIs/Auth/authMe.ts
+++ src/Mohassel/Services/APIs/Auth/authMe.ts
@@ -5,7 +5,7 @@ export const authMe = async () => {
     try {
         const res = await axios.get(url);
         if (res.data.validBranches && res.data.validBranches.length > 0) {
-            document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_DOMAIN ? `;domain=${process.env.REACT_APP_DOMAIN}` : '' + ';path=/;');
+            document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_DOMAIN ? `;domain=${process.env.REACT_APP_DOMAIN}` : '') + ';path=/;';
         }
         return { status: "success", body: res.data }
     }
