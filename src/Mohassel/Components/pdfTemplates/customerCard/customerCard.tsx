cdf7f68ba1e9a7ebd7aa76395f4fddc6527cfd38
Sara Alaa <sara3laa.93@gmail.com>
2020-11-11 16:05:31 +0200

    space between license  and logo

--- src/Mohassel/Components/pdfTemplates/customerCard/customerCard.tsx
+++ src/Mohassel/Components/pdfTemplates/customerCard/customerCard.tsx
@@ -58,7 +58,7 @@ class CustomerCardPDF extends Component<Props, State> {
                 <div className="customer-card-print" style={{ direction: "rtl" }} lang="ar">
                 <table style={{ fontSize: "12px", margin: "10px 0px", textAlign: "center", width: '100%' }}>
                     <tr style={{ height: "10px" }}></tr>
-                    <tr><th style = {{backgroundColor: 'white'}} colSpan={1}><img style={{ width: "70px", height: "35px" }} src={require('../../../../Shared/Assets/Logo.svg')} /></th><th  style = {{backgroundColor: 'white'}} colSpan={6}>ترخيص ممارسه نشاط التمويل متناهي الصغر رقم (2) لسنه 2015</th></tr>
+                    <tr style={{width:'100%',display:'flex',flexDirection:'row' , justifyContent:'space-between'}}><th style = {{backgroundColor: 'white'}} colSpan={6}><img style={{ width: "70px", height: "35px" }} src={require('../../../../Shared/Assets/Logo.svg')} /></th><th  style = {{backgroundColor: 'white'}} colSpan={6}>ترخيص ممارسه نشاط التمويل متناهي الصغر رقم (2) لسنه 2015</th></tr>
                     <tr style={{ height: "10px" }}></tr>
                 </table>
                     <table>
--- src/Mohassel/Components/pdfTemplates/finesTransaction/finesTransaction.tsx
+++ src/Mohassel/Components/pdfTemplates/finesTransaction/finesTransaction.tsx
@@ -5,7 +5,7 @@ const FinesTransaction = () => {
             <div className="fines-transaction" lang="ar">
             <table style={{ fontSize: "12px", margin: "10px 0px", textAlign: "center", width: '100%' }}>
                 <tr style={{ height: "10px" }}></tr>
-                <tr><th   style={{backgroundColor:'white'}}  colSpan={1}><img style={{ width: "70px", height: "35px" }} src={require('../../../../Shared/Assets/Logo.svg')} /></th><th style={{backgroundColor:'white'}} colSpan={6}>ترخيص ممارسه نشاط التمويل متناهي الصغر رقم (2) لسنه 2015</th></tr>
+                <tr style={{width:'100%',display:'flex',flexDirection:'row' , justifyContent:'space-between'}}><th style={{backgroundColor:'white'}}  colSpan={6}><img style={{ width: "70px", height: "35px" }} src={require('../../../../Shared/Assets/Logo.svg')} /></th><th style={{backgroundColor:'white'}} colSpan={6}>ترخيص ممارسه نشاط التمويل متناهي الصغر رقم (2) لسنه 2015</th></tr>
                 <tr style={{ height: "10px" }}></tr>
             </table>
                 <table className="report-container">
