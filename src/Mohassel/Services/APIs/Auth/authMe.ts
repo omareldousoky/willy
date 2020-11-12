a0b832035147ca5d34c86346617a981a19072243
Ahmed Fahmy <afahmy31@gmail.com>
2020-11-12 12:11:54 +0200

    clear ltsbranch if no branch

--- src/Mohassel/Services/APIs/Auth/authMe.ts
+++ src/Mohassel/Services/APIs/Auth/authMe.ts
@@ -6,6 +6,8 @@ export const authMe = async () => {
         const res = await axios.get(url);
         if (res.data.validBranches && res.data.validBranches.length > 0) {
             document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_LTS_SUBDOMAIN ? `;domain=${process.env.REACT_APP_LTS_SUBDOMAIN}` : '' + ';path=/;');
+        } else {
+            document.cookie = "ltsbranch=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
         }
         return { status: "success", body: res.data }
     }
