2932bdbea91bc1d73d3db323d53ef8ac24d7f61b
Ahmed Fahmy <afahmy31@gmail.com>
2020-11-12 14:00:07 +0200

    fix changing branch on referesh

--- src/Mohassel/Services/APIs/Auth/authMe.ts
+++ src/Mohassel/Services/APIs/Auth/authMe.ts
@@ -1,13 +1,16 @@
 import axios from '../axios-instance';
+import { getCookie } from '../../getCookie';
 
 export const authMe = async () => {
     const url = process.env.REACT_APP_BASE_URL + '/auth/me';
     try {
         const res = await axios.get(url);
-        if (res.data.validBranches && res.data.validBranches.length > 0) {
-            document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_DOMAIN ? `;domain=${process.env.REACT_APP_DOMAIN}` : '') + ';path=/;';
-        } else {
-            document.cookie = `ltsbranch=;domain=${process.env.REACT_APP_DOMAIN}` + ';path=/;';
+        if (!getCookie('ltsbranch')) {
+            if (res.data.validBranches && res.data.validBranches.length > 0) {
+                document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_DOMAIN ? `;domain=${process.env.REACT_APP_DOMAIN}` : '') + ';path=/;';
+            } else {
+                document.cookie = `ltsbranch=;domain=${process.env.REACT_APP_DOMAIN}` + ';path=/;';
+            }
         }
         return { status: "success", body: res.data }
     }
