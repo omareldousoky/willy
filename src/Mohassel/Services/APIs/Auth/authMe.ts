f7218278a494205e6f6df3f28cbbc906816f6e26
Ahmed Fahmy <afahmy31@gmail.com>
2020-11-12 12:21:30 +0200

    change cookie

--- src/Mohassel/Services/APIs/Auth/authMe.ts
+++ src/Mohassel/Services/APIs/Auth/authMe.ts
@@ -5,7 +5,7 @@ export const authMe = async () => {
     try {
         const res = await axios.get(url);
         if (res.data.validBranches && res.data.validBranches.length > 0) {
-            document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_DOMAIN ? `;domain=${process.env.REACT_APP_DOMAIN}` : '' + ';path=/;');
+            document.cookie = 'ltsbranch=' + JSON.stringify(res.data.validBranches[0]) + (process.env.REACT_APP_DOMAIN ? `;domain=${process.env.REACT_APP_DOMAIN}` : '') + ';path=/;';
         } else {
             document.cookie = "ltsbranch=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
         }
