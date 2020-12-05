62106bceb65c655dbf105283980948ba96be8a38
Sara Alaa <sara3laa.93@gmail.com>
2020-12-05 23:24:15 +0200

    add apis

---
+++ src/Mohassel/Services/APIs/DeathCerificate/uploadDeathCertificate.ts
@@ -0,0 +1,12 @@
+import axios from '../axios-instance';
+
+export const uploadDeathCertificate = async (data: FormData) => {
+    const url = process.env.REACT_APP_BASE_URL + 'customer/death-certificate';
+    try {
+        const res = await axios.post(url, data);
+        return { status: "success", body: res.data }
+    }
+    catch (error) {
+        return { status: "error", error: error.response.data }
+    }
+}
