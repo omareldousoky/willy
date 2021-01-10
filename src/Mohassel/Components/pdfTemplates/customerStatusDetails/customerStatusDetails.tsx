48a9127662497df049aba00cbcfc52249e6c829f
Sara Alaa <sara3laa.93@gmail.com>
2021-01-10 16:33:53 +0200

    WIP

--- src/Mohassel/Components/pdfTemplates/customerStatusDetails/customerStatusDetails.tsx
+++ src/Mohassel/Components/pdfTemplates/customerStatusDetails/customerStatusDetails.tsx
@@ -285,7 +285,7 @@ const CustomerStatusDetails = (props) => {
                                                         <td>{numbersToArabic(loan.totalPaid)}</td>
                                                         <td>{numbersToArabic(loan.totalFeesPaid)}</td>
                                                         <th>رصيد العميل</th>
-                                                        <td>{loan.totalPaid? numbersToArabic((loanTotalInstallments-Number(loan.totalPaid)).toFixed(2)): numbersToArabic(loanTotalInstallments.toFixed(2))}</td>
+                                                        <td>{loan.status==='pening'|| 'issued' ? props.data.remainingTotla :0}</td>
                                                         <th>أيام التأخير </th>
                                                         <td>{numbersToArabic(loan.lateDays)}</td>
                                                         <th> أيام التبكير </th>
---
+++ src/Mohassel/Services/APIs/Loan/remainingLoan.ts
@@ -0,0 +1,12 @@
+import axios from '../axios-instance';
+
+export const remainingLoan = async (id: string) => {
+    const url = process.env.REACT_APP_BASE_URL + `/loan/remaining-loan?customerId=${id}`;
+    try {
+        const res = await axios.get(url);
+        return { status: "success", body: res.data }
+    }
+    catch (error) {
+        return { status: "error", error: error.response.data }
+    }
+}
