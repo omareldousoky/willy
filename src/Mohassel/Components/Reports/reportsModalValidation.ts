19071eec2c17b33f54a387a59c444adb0b3e6def
Sara Alaa <sara3laa.93@gmail.com>
2021-01-21 13:09:41 +0200

    date validation

--- src/Mohassel/Components/Reports/reportsModalValidation.ts
+++ src/Mohassel/Components/Reports/reportsModalValidation.ts
@@ -1,9 +1,14 @@
 import * as Yup from 'yup';
 import * as local from '../../../Shared/Assets/ar.json';
 
+const currentYear =   new Date(2020,0,1);
 export const reportsModalValidation = Yup.object().shape({
     fromDate: Yup.string(),
     toDate: Yup.string(),
     branches: Yup.array().nullable(),
     key: Yup.string(),
+    quarterYear:  Yup.string().test(
+        "Max Date", local.dateShouldBeBeforeToday,
+        (value: any) => { return value ? new Date(value).valueOf() >= currentYear.valueOf() : true }
+    ).required(local.required),
 })
