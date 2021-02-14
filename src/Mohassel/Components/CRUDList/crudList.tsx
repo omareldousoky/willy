b795ae1d50fe5ae0058c4487e2ee0754287fff64
Omar Eldousoky <omareldousoky@gmail.com>
2021-02-14 16:25:52 +0200

    component done

--- src/Mohassel/Components/CRUDList/crudList.tsx
+++ src/Mohassel/Components/CRUDList/crudList.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -14,7 +14,6 @@ interface Option {
 interface Props {
     source: string;
     options: Array<Option>;
-    addOption: Function;
     newOption: Function;
     updateOption: Function;
 }
@@ -25,163 +24,119 @@ interface State {
     temp: Array<string>;
     source: string;
 }
-class CRUDList extends Component<Props, State> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            options: [],
-            loading: false,
-            filterOptions: '',
-            source: '',
-            temp: []
+export const CRUDList = (props: Props) => {
+    const [options, setOptions] = useState<Array<Option>>(props.options);
+    const [filterOptions, setFilterOptions] = useState('');
+    const [temp, setTemp] = useState<Array<Option>>(props.options);
+    useEffect(() => {
+        setOptions(props.options);
+        setTemp(props.options)
+    }, [props.options]);
+    function addOption() {
+        if (!options.some(option => option.name === "")) {
+            setOptions([{ name: "", disabledUi: false, id: "", activated: true }, ...options]);
+            setFilterOptions('');
+            setTemp([{ name: "", disabledUi: false, id: "", activated: true }, ...temp])
         }
     }
-    static getDerivedStateFromProps(props, state) {
-        if (props.source !== state.source || JSON.stringify(props.options) !== JSON.stringify(state.options)) {
-            console.log('UPDATEEEE1')
-            return {
-                source: props.source,
-                options: props.options,
-                temp: props.options.map(option => option.name)
-            }
-        }
-        else {
-            return null
-        }
+    function handleChangeInput(event: React.ChangeEvent<HTMLInputElement>, index: number) {
+        setOptions(options.map((option, optionIndex) => optionIndex === index ? { ...option, name: event.currentTarget.value } : option))
     }
-    // componentDidUpdate(prevProps) {
-    //     if (this.props.source !== prevProps.source) {
-    //         console.log('UPDATEEEE2')
-    //         this.setState({ source: this.props.source })
-    //     }
-    // }
-    componentDidMount() {
-    //     this.setState({ loading: true });
-    //     const res = await getLoanUsage();
-    //     if (res.status === "success") {
-    //         const responseLoanUsages = res.body.usages.map(usage => ({ ...usage, disabledUi: true }));
-            this.setState({
-                // loading: false,
-                options: this.props.options,
-                temp: this.props.options.map(usage => usage.name)
-            });
-    //     } else {
-    //         this.setState({ loading: false }, () => Swal.fire("Error !", getErrorMessage(res.error.error), 'error'));
-    //     }
-    }
-    addOption() {
-        if (!this.state.options.some(option => option.name === "")) {
-            this.props.addOption()
-        //     this.setState({
-        //         filterOptions: '',
-        //         options: [...this.state.options, { name: "", disabledUi: false, id: "", activated: true }],
-        //         temp: [...this.state.temp, '']
-        //     })
-        }
-    }
-    handleChangeInput(event: React.ChangeEvent<HTMLInputElement>, index: number) {
-        this.setState({
-            options: this.state.options.map((option, optionIndex) => optionIndex === index ? { ...option, name: event.currentTarget.value } : option)
-        })
-    }
-    handleKeyDown(event: React.KeyboardEvent, index: number) {
-        if (event.key === 'Enter') {
-            this.toggleClick(index, true)
-        }
-    }
-    async toggleClick(index: number, submit: boolean) {
-        console.log('Click', submit, this.state.options[index])
-        if (this.state.options[index].disabledUi === false && this.state.options[index].name.trim() !== "" && submit) {
-            if (this.state.options[index].id === "") {
+    function toggleClick(index: number, submit: boolean) {
+        if (options[index].disabledUi === false && options[index].name.trim() !== "" && submit) {
+            if (options[index].id === "") {
                 //New 
-                this.props.newOption(this.state.options[index].name, this.state.options[index].activated, index);
+                props.newOption(options[index].name, options[index].activated, index);
             } else {
                 //Edit 
-                this.props.updateOption(this.state.options[index].id, this.state.options[index].name, this.state.options[index].activated, index);
+                props.updateOption(options[index].id, options[index].name, options[index].activated, index);
             }
         } else if (!submit) {
-            const options = this.state.options;
-            options[index].disabledUi = !options[index].disabledUi;
-            this.setState({ options })
+            const optionsTemp = [...options];
+            optionsTemp[index].disabledUi = !options[index].disabledUi;
+            setOptions(optionsTemp)
         }
     }
-    render() {
-        return (
-
-            <Container style={{ marginTop: 20 }}>
-                <div style={{ display: 'flex', textAlign: 'center', justifyContent: 'center', alignItems: 'center', marginBottom: 20 }}>
-                    <Form.Control
-                        type="text"
-                        data-qc="filterOptions"
-                        placeholder={local.search}
-                        maxLength={100}
-                        value={this.state.filterOptions}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({ filterOptions: e.currentTarget.value })}
-                    />
-                    {/* <h4 style={{ textAlign: 'right' }}>{local.options}</h4> */}
-                    <span
-                        onClick={() => this.addOption()}
-                        className="fa fa-plus fa-lg"
-                        style={{ margin: '0px 20px', color: '#7dc356', cursor: 'pointer' }}
-                    />
-                </div>
-                <ListGroup style={{ textAlign: 'right', width: '30%', marginBottom: 30 }}>
-                    <Loader type="fullsection" open={this.state.loading} />
-                    {console.log(this.state.options)}
-                    {this.state.options
-                        .filter(option => option.name.toLocaleLowerCase().includes(this.state.filterOptions.toLocaleLowerCase()))
-                        .map((option, index) => {
-                            return (
-                                <ListGroup.Item key={index} style={{ display: 'flex', alignItems: 'center' }}>
-                                    <Form.Group style={{ margin: '0px 0px 0px 20px' }}>
-                                        <Form.Control
-                                            type="text"
-                                            data-qc="loanUsageInput"
-                                            maxLength={100}
-                                            title={option.name}
-                                            value={option.name}
-                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.handleChangeInput(e, index)}
-                                            onKeyDown={(e: React.KeyboardEvent) => this.handleKeyDown(e, index)}
-                                            disabled={option.disabledUi}
-                                            style={option.disabledUi ? { background: 'none', border: 'none' } : {}}
-                                            isInvalid={this.state.options[index].name.trim() === ""}
-                                        />
-                                        <Form.Control.Feedback type="invalid">
-                                            {local.required}
-                                        </Form.Control.Feedback>
-                                    </Form.Group>
-                                    {option.disabledUi ?
-                                        <span
-                                            style={option.activated ? { color: '#7dc356', marginLeft: 20 } : { color: '#d51b1b', marginLeft: 20 }}
-                                            className={option.activated ? "fa fa-check-circle fa-lg" : "fa fa-times-circle fa-lg"} />
-                                        :
-                                        <>
-                                            <Form.Check
-                                                type="checkbox"
-                                                data-qc={`activate${index}`}
-                                                label={local.active}
-                                                className="checkbox-label"
-                                                checked={this.state.options[index].activated}
-                                                onChange={() => this.setState({ options: this.state.options.map((option, optionIndex) => optionIndex === index ? { ...option, activated: !this.state.options[index].activated } : option) })}
-                                            />
-                                            <span className="fa fa-undo fa-lg"
-                                                style={{ color: '#7dc356', cursor: 'pointer', marginLeft: 20 }}
-                                                onClick={() => this.state.temp[index] !== '' ? this.setState({ options: this.state.options.map((option, optionIndex) => optionIndex === index ? { ...option, name: this.state.temp[index], disabledUi: true } : option) }) : this.setState({ options: this.state.options.filter(loanItem => loanItem.id !== "") })}
-                                            />
-                                        </>
-                                    }
-                                    <span
-                                        onClick={() => option.disabledUi ? this.toggleClick(index, false) : this.toggleClick(index, true)}
-                                        style={{ color: '#7dc356', cursor: 'pointer', marginLeft: 20 }}
-                                        data-qc="editSaveIcon"
-                                        className={option.disabledUi ? "fa fa-edit fa-lg" : "fa fa-save fa-lg"} />
-                                </ListGroup.Item>
-                            )
-                        }).reverse()}
-                </ListGroup>
-            </Container>
-        );
+    function handleKeyDown(event: React.KeyboardEvent, index: number) {
+        if (event.key === 'Enter') {
+            toggleClick(index, true)
+        }
     }
-}
+    function reset(index) {
+        const optionsTemp = [...options];
+        optionsTemp[index] = { ...temp[index], disabledUi: true };
+        setOptions(optionsTemp)
+    }
+    return (
 
-export default CRUDList;
+        <Container style={{ marginTop: 20 }}>
+            <div style={{ display: 'flex', textAlign: 'center', justifyContent: 'center', alignItems: 'center', marginBottom: 20 }}>
+                <Form.Control
+                    type="text"
+                    data-qc="filterOptions"
+                    placeholder={local.search}
+                    maxLength={100}
+                    value={filterOptions}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFilterOptions(e.currentTarget.value)}
+                />
+                <span
+                    onClick={() => addOption()}
+                    className="fa fa-plus fa-lg"
+                    style={{ margin: '0px 20px', color: '#7dc356', cursor: 'pointer' }}
+                />
+            </div>
+            <ListGroup style={{ textAlign: 'right', width: '30%', marginBottom: 30 }}>
+                {options
+                    .filter(option => option.name.toLocaleLowerCase().includes(filterOptions.toLocaleLowerCase()))
+                    .map((option, index) => {
+                        return (
+                            <ListGroup.Item key={index} style={{ display: 'flex', alignItems: 'center' }}>
+                                <Form.Group style={{ margin: '0px 0px 0px 20px' }}>
+                                    <Form.Control
+                                        type="text"
+                                        data-qc="loanUsageInput"
+                                        maxLength={100}
+                                        title={option.name}
+                                        value={option.name}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChangeInput(e, index)}
+                                        onKeyDown={(e: React.KeyboardEvent) => handleKeyDown(e, index)}
+                                        disabled={option.disabledUi}
+                                        style={option.disabledUi ? { background: 'none', border: 'none' } : {}}
+                                        isInvalid={options[index].name.trim() === ""}
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {local.required}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                {option.disabledUi ?
+                                    <span
+                                        style={option.activated ? { color: '#7dc356', marginLeft: 20 } : { color: '#d51b1b', marginLeft: 20 }}
+                                        className={option.activated ? "fa fa-check-circle fa-lg" : "fa fa-times-circle fa-lg"} />
+                                    :
+                                    <>
+                                        <Form.Check
+                                            type="checkbox"
+                                            data-qc={`activate${index}`}
+                                            label={local.active}
+                                            className="checkbox-label"
+                                            checked={options[index].activated}
+                                            onChange={() => setOptions(options.map((option, optionIndex) => optionIndex === index ? { ...option, activated: !options[index].activated } : option))}
+                                        />
+                                        <span className="fa fa-undo fa-lg"
+                                            style={{ color: '#7dc356', cursor: 'pointer', marginLeft: 20 }}
+                                            onClick={() => temp[index].name !== '' ? reset(index) : setOptions(options.filter(loanItem => loanItem.id !== ""))}
+                                        />
+                                    </>
+                                }
+                                <span
+                                    onClick={() => option.disabledUi ? toggleClick(index, false) : toggleClick(index, true)}
+                                    style={{ color: '#7dc356', cursor: 'pointer', marginLeft: 20 }}
+                                    data-qc="editSaveIcon"
+                                    className={option.disabledUi ? "fa fa-edit fa-lg" : "fa fa-save fa-lg"} />
+                            </ListGroup.Item>
+                        )
+                    })}
+            </ListGroup>
+        </Container>
+    );
+}
--- src/Mohassel/Components/LoanUses/loanUses.tsx
+++ src/Mohassel/Components/LoanUses/loanUses.tsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import { Loader } from '../../../Shared/Components/Loader';
 import { getLoanUsage } from '../../Services/APIs/LoanUsage/getLoanUsage';
 import { addLoanUsage } from '../../Services/APIs/LoanUsage/addLoanUsage';
@@ -11,7 +8,7 @@ import HeaderWithCards from '../HeaderWithCards/headerWithCards';
 import { manageLoanDetailsArray } from '../ManageLoanDetails/manageLoanDetailsInitials';
 import Swal from 'sweetalert2';
 import { getErrorMessage } from '../../../Shared/Services/utils';
-import CRUDList from '../CRUDList/crudList';
+import {CRUDList} from '../CRUDList/crudList';
 
 interface LoanUse {
   name: string;
@@ -46,55 +43,18 @@ class LoanUses extends Component<{}, State> {
       const responseLoanUsages = res.body.usages.map(usage => ({ ...usage, disabledUi: true }));
       this.setState({
         loading: false,
-        loanUses: responseLoanUsages,
+        loanUses: responseLoanUsages.reverse(),
         temp: res.body.usages.map(usage => usage.name)
       });
     } else {
       this.setState({ loading: false }, () => Swal.fire("Error !", getErrorMessage(res.error.error), 'error'));
     }
   }
-  // addLoanUse() {
-  //   if (!this.state.loanUses.some(loanUse => loanUse.name === "")) {
-  //     this.setState({
-  //       filterLoanUsage: '',
-  //       loanUses: [...this.state.loanUses, { name: "", disabledUi: false, id: "", activated: true }],
-  //       temp: [...this.state.temp, '']
-  //     })
-  //   }
-  // }
-  // handleChangeInput(event: React.ChangeEvent<HTMLInputElement>, index: number) {
-  //   this.setState({
-  //     loanUses: this.state.loanUses.map((loanUse, loanUseIndex) => loanUseIndex === index ? { ...loanUse, name: event.currentTarget.value } : loanUse)
-  //   })
-  // }
-  // handleKeyDown(event: React.KeyboardEvent, index: number) {
-  //   if (event.key === 'Enter') {
-  //     this.toggleClick(index, true)
-  //   }
-  // }
-  // async toggleClick(index: number, submit: boolean) {
-  //   if (this.state.loanUses[index].disabledUi === false && this.state.loanUses[index].name.trim() !== "") {
-  //     if (this.state.loanUses[index].id === "") {
-  //       //New 
-
-  //     } else {
-  //       //Edit 
-
-  //     }
-  //   } else if (!submit) {
-  //     this.setState({
-  //       loanUses: this.state.loanUses.map((loanUse, loanUseIndex) => loanUseIndex === index ? { ...loanUse, disabledUi: !loanUse.disabledUi } : loanUse)
-  //     })
-  //   }
-  // }
   async editLoanUse(id, name, active, index) {
     this.setState({ loading: true })
     const res = await updateLoanUsage(id, name, active);
     if (res.status === "success") {
-      const options = this.state.loanUses;
-      options[index].disabledUi = !options[index].disabledUi;
       this.setState({
-        loanUses: options,
         loading: false,
       }, () => this.getUses())
     } else this.setState({ loading: false }, () => Swal.fire("Error !", getErrorMessage(res.error.error), 'error'))
@@ -103,10 +63,7 @@ class LoanUses extends Component<{}, State> {
     this.setState({ loading: true })
     const res = await addLoanUsage({ name: name, activated: activated });
     if (res.status === "success") {
-      const options = this.state.loanUses;
-      options[index].disabledUi = !options[index].disabledUi;
       this.setState({
-        loanUses: options,
         loading: false,
       }, () => this.getUses())
     } else this.setState({ loading: false }, () => Swal.fire("Error !", getErrorMessage(res.error.error), 'error'))
@@ -122,83 +79,8 @@ class LoanUses extends Component<{}, State> {
           active={array.map(item => { return item.icon }).indexOf('loanUses')}
         />
         <CRUDList source={'loanUses'} options={this.state.loanUses}
-          addOption={() => {
-            console.log('addrow')
-            this.setState({
-              loanUses: [...this.state.loanUses, { name: "", disabledUi: false, id: "", activated: true }],
-            })
-          }}
           newOption={(name, active, index) => { this.newLoanUse(name, active, index) }}
           updateOption={(id, name, active, index) => { this.editLoanUse(id, name, active, index) }} />
-        {/* <Container style={{ marginTop: 20 }}>
-          <div style={{ display: 'flex', textAlign: 'center', justifyContent: 'center', alignItems: 'center', marginBottom: 20 }}>
-            <Form.Control
-              type="text"
-              data-qc="filterLoanUsage"
-              placeholder={local.search}
-              maxLength={100}
-              value={this.state.filterLoanUsage}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.setState({ filterLoanUsage: e.currentTarget.value })}
-            />
-            <span
-              onClick={() => this.addLoanUse()}
-              className="fa fa-plus fa-lg"
-              style={{ margin: '0px 20px', color: '#7dc356', cursor: 'pointer' }}
-            />
-          </div>
-          <ListGroup style={{ textAlign: 'right', width: '30%', marginBottom: 30 }}>
-            {this.state.loanUses
-              .filter(loanUse => loanUse.name.toLocaleLowerCase().includes(this.state.filterLoanUsage.toLocaleLowerCase()))
-              .map((loanUse, index) => {
-                return (
-                  <ListGroup.Item key={index} style={{ display: 'flex', alignItems: 'center' }}>
-                    <Form.Group style={{ margin: '0px 0px 0px 20px' }}>
-                      <Form.Control
-                        type="text"
-                        data-qc="loanUsageInput"
-                        maxLength={100}
-                        title={loanUse.name}
-                        value={loanUse.name}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.handleChangeInput(e, index)}
-                        onKeyDown={(e: React.KeyboardEvent) => this.handleKeyDown(e, index)}
-                        disabled={loanUse.disabledUi}
-                        style={loanUse.disabledUi ? { background: 'none', border: 'none' } : {}}
-                        isInvalid={this.state.loanUses[index].name.trim() === ""}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {local.required}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                    {loanUse.disabledUi ?
-                      <span
-                        style={loanUse.activated ? { color: '#7dc356', marginLeft: 20 } : { color: '#d51b1b', marginLeft: 20 }}
-                        className={loanUse.activated ? "fa fa-check-circle fa-lg" : "fa fa-times-circle fa-lg"} />
-                      :
-                      <>
-                        <Form.Check
-                          type="checkbox"
-                          data-qc={`activate${index}`}
-                          label={local.active}
-                          className="checkbox-label"
-                          checked={this.state.loanUses[index].activated}
-                          onChange={() => this.setState({ loanUses: this.state.loanUses.map((loanUse, loanUseIndex) => loanUseIndex === index ? { ...loanUse, activated: !this.state.loanUses[index].activated } : loanUse) })}
-                        />
-                        <span className="fa fa-undo fa-lg"
-                          style={{ color: '#7dc356', cursor: 'pointer', marginLeft: 20 }}
-                          onClick={() => this.state.temp[index] !== '' ? this.setState({ loanUses: this.state.loanUses.map((loanUse, loanUseIndex) => loanUseIndex === index ? { ...loanUse, name: this.state.temp[index], disabledUi: true } : loanUse) }) : this.setState({ loanUses: this.state.loanUses.filter(loanItem => loanItem.id !== "") })}
-                        />
-                      </>
-                    }
-                    <span
-                      onClick={() => loanUse.disabledUi ? this.toggleClick(index, false) : this.toggleClick(index, true)}
-                      style={{ color: '#7dc356', cursor: 'pointer', marginLeft: 20 }}
-                      data-qc="editSaveIcon"
-                      className={loanUse.disabledUi ? "fa fa-edit fa-lg" : "fa fa-save fa-lg"} />
-                  </ListGroup.Item>
-                )
-              }).reverse()}
-          </ListGroup>
-        </Container> */}
       </>
     );
   }
