20936c47e2a455d6ea6ce5f67068b5debe3a0e42
Sara Alaa <sara3laa.93@gmail.com>
2021-02-18 00:47:48 +0200

    wip

--- src/Mohassel/Components/pdfTemplates/activeWalletIndividual/activeWalletIndividual.tsx
+++ src/Mohassel/Components/pdfTemplates/activeWalletIndividual/activeWalletIndividual.tsx
@@ -3,127 +3,195 @@ import './activeWalletIndividual.scss';
 import Orientation from '../../Common/orientation';
 import store from "../../../../Shared/redux/store";
 import {
-  numbersToArabic,
-  timeToArabicDate,
-  timeToDate,
+    numbersToArabic,
+    timeToArabicDate,
+    timeToDate,
 } from "../../../../Shared/Services/utils";
 interface ActiveWalletIndividualResponse {
-  response: {
-    branchName: string;
-    totalLoanCount: number;
-    totalPrincipal: number;
-    customersCount: number;
-    totalCreditInstallmentCount: number;
-    totalCreditInstallmentAmount: number;
-    totalLateInstallmentCount: number;
-    totalLateInstallmentAmount: number;
-    data:
-    {
-      phoneNumber: string;
-      workArea: string;
-      address: string;
-      activity: string;
-      creditAmount: number;
-      creditCount: number;
-      firstLateDate: string;
-      latestPaymentDate: string;
-      lateAmount: number;
-      lateCount: number;
-      principal: number;
-      loanDate: string;
-      customerName: string;
-      customerCode: number;
+    response: {
+        branchName: string;
+        totalLoanCount: number;
+        totalPrincipal: number;
+        customersCount: number;
+        totalCreditInstallmentCount: number;
+        totalCreditInstallmentAmount: number;
+        totalLateInstallmentCount: number;
+        totalLateInstallmentAmount: number;
+        officers: {
+            officerName: string;
+            customersCount: number;
+            totalCreditInstallmentAmount: number;
+            totalCreditInstallmentCount: number;
+            totalLateInstallmentAmount: number;
+            totalLateInstallmentCount: number;
+            totalLoanCount: number;
+            totalPrincipal: number;
+            areas: {
+                areaName: string;
+                customersCount: string;
+                totalCreditInstallmentAmount: number;
+                totalCreditInstallmentCount: number;
+                totalLateInstallmentAmount: number;
+                totalLateInstallmentCount: number;
+                totalLoanCount: number;
+                totalPrincipal: number;
+                data: {
+                    phoneNumber: string;
+                    workArea: string;
+                    address: string;
+                    activity: string;
+                    creditAmount: number;
+                    creditCount: number;
+                    firstLateDate: string;
+                    latestPaymentDate: string;
+                    lateAmount: number;
+                    lateCount: number;
+                    principal: number;
+                    loanDate: string;
+                    customerName: string;
+                    customerCode: number;
+                    sector: string;
+                }[];
+            }[];
+        }[];
     }[];
-  }[];
 }
 interface Props {
-  date: string;
-  data: ActiveWalletIndividualResponse;
+    date: string;
+    data: ActiveWalletIndividualResponse;
 }
 const ActiveWalletIndividual = (props: Props) => {
-  const { data } = props;
-  return (
-    <>
-      <Orientation size="landscape" />
-      <div className={"activeWalletIndividual"}>
-        <div className="header-wrapper">
-          <span className="logo-print" role="img" />
-          <p className="m-0">
-            ترخيص ممارسه نشاط التمويل متناهي الصغر رقم (2) لسنه 2015
+    const { data } = props;
+    return (
+        <>
+            <Orientation size="landscape" />
+            <div className={"activeWalletIndividual"}>
+                <div className="header-wrapper">
+                    <span className="logo-print" role="img" />
+                    <p className="m-0">
+                        ترخيص ممارسه نشاط التمويل متناهي الصغر رقم (2) لسنه 2015
           </p>
-        </div>
-        <div className="header-wrapper mb-0">
-          <p style={{ marginRight: "10px" }}>
-            شركة تساهيل للتمويل متناهي الصغر
+                </div>
+                <div className="header-wrapper mb-0">
+                    <p style={{ marginRight: "10px" }}>
+                        شركة تساهيل للتمويل متناهي الصغر
           </p>
-          <h3>المحفظة النشطه للمندوبين - فردى</h3>
-          <div className="col-nowrap">
-            <p>{store.getState().auth.name}</p>
-            <p>{new Date().toLocaleString('en-GB', { hour12: false })}</p>
-          </div>
-        </div>
-        {data?.response?.map((branch, index) => {
-          return (
-            <React.Fragment key={index}>
-              <div className="report-header">
-                <div className="row-nowrap mt-1"><p>الفرع:</p><p className="box">{'--'}</p></div>
-                <div className="row-nowrap"><p>المندوب:</p> <p className="box small-box"></p><p className="box">{'--'}</p></div>
-                <div className="row-nowrap"><p>منطقة العمل:</p><p className="box">{'--'}</p></div>
-              </div>
-              <table className="report-container">
-                <thead>
-                  <tr>
-                    <th colSpan={6}></th>
-                    <th colSpan={3}>الرصيد</th>
-                    <th colSpan={2}></th>
-                    <th colSpan={5}>متأخرات</th>
-                    <th colSpan={6}>بيانات رئيسة المجموعه</th>
-                  </tr>
-                  <tr>
-                    <th colSpan={4}>المجموعه</th>
-                    <th colSpan={2}> ت التمويل</th>
-                    <th colSpan={2}>قيمة التمويل</th>
-                    <th>عدد</th>
-                    <th>مبلغ</th>
-                    <th colSpan={2}> ت أخر سداد</th>
-                    <th>عدد</th>
-                    <th colSpan={2}>مبلغ</th>
-                    <th colSpan={2}>بداية تاخير</th>
-                    <th>نشاط ر. مجموعه</th>
-                    <th colSpan={3}>عنوان ر. مجموعه</th>
-                    <th>منطقة العمل</th>
-                    <th>التليفون</th>
-                  </tr>
-                </thead>
-                {branch.data?.map((row, i) => {
-                  return (
-                    <>
-                      <tbody key={i}>
-                        <td colSpan={4}>{row.customerName}</td>
-                        <td colSpan={2}>{row.loanDate}</td>
-                        <td>{row.principal}</td>
-                        <td>{row.creditCount}</td>
-                        <td>{row.creditAmount}</td>
-                        <td>{row.lateCount}</td>
-                        <td>{row.lateAmount}</td>
-                        <td colSpan={2}>{row.latestPaymentDate}</td>
-                        <td>{row.firstLateDate}</td>
-                        <td>{row.activity}</td>
-                        <td colSpan={3}>{row.address}</td>
-                        <td>{row.workArea}</td>
-                        <td>{row.phoneNumber}</td>
-                      </tbody>
-                      <tr><p>تعديل:</p></tr>
-                    </>
-                  )
-                })}
-              </table>
-            </React.Fragment>
-          )
-        })}
-      </div>
-    </>
-  )
+                    <h3>المحفظة النشطه للمندوبين - فردى</h3>
+                    <div className="col-nowrap">
+                        <p>{store.getState().auth.name}</p>
+                        <p>{new Date().toLocaleString('en-GB', { hour12: false })}</p>
+                    </div>
+                </div>
+                {
+                    data?.response?.map((branch, branchIndex) => {
+                        return (
+
+                            <React.Fragment key={branchIndex}>
+                                <div className="row-nowrap mt-1"><p>الفرع:</p><p className="box">{branch.branchName || '--'}</p></div>
+
+                                { branch?.officers?.map((officer, officerIndex) => {
+                                    return (
+                                        <React.Fragment key={officerIndex} >
+                                            <div className="row-nowrap"><p>المندوب:</p><p className="box">{officer.officerName || '--'}</p></div>
+                                            {
+                                                officer?.areas?.map((area, areaIndex) => {
+
+                                                    return (
+                                                        <React.Fragment key={areaIndex}>
+                                                            <div className="row-nowrap"><p>منطقة العمل:</p><p className="box">{area.areaName || '--'}</p></div>
+                                                            <table
+                                                                className="report-container"
+                                                                cellPadding="2"
+                                                                cellSpacing="2"
+                                                            >
+                                                                <thead>
+                                                                    <tr>
+                                                                        <th colSpan={9}></th>
+                                                                        <th colSpan={3}>الرصيد</th>
+                                                                        <th colSpan={2}></th>
+                                                                        <th colSpan={5}>متأخرات</th>
+                                                                        <th colSpan={7}>بيانات رئيسة المجموعه</th>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <th colSpan={5}>المجموعه</th>
+                                                                        <th colSpan={2}> ت التمويل</th>
+                                                                        <th colSpan={2}>قيمة التمويل</th>
+                                                                        <th>عدد</th>
+                                                                        <th colSpan={2}>مبلغ</th>
+                                                                        <th colSpan={2}> ت أخر سداد</th>
+                                                                        <th>عدد</th>
+                                                                        <th colSpan={2}>مبلغ</th>
+                                                                        <th colSpan={2}>بداية تاخير</th>
+                                                                        <th>نشاط ر. مجموعه</th>
+                                                                        <th colSpan={3}>عنوان ر. مجموعه</th>
+                                                                        <th>منطقة العمل</th>
+                                                                        <th colSpan={2}>التليفون</th>
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                    {
+
+                                                                        area?.data?.map((row, rowIndex) => {
+                                                                            return (
+                                                                                <React.Fragment key={rowIndex}>
+                                                                                    <tr>
+                                                                                        <td colSpan={5}>{`${row.customerCode} - ${row.customerName}`}</td>
+                                                                                        <td colSpan={2}>{row.loanDate || '--'}</td>
+                                                                                        <td colSpan={2}>{row.principal || 0}</td>
+                                                                                        <td>{row.creditCount || 0}</td>
+                                                                                        <td colSpan={2}>{row.creditAmount || 0}</td>
+                                                                                        <td colSpan={2}>{row.latestPaymentDate || '--'}</td>
+                                                                                        <td>{row.lateCount || 0}</td>
+                                                                                        <td colSpan={2}>{row.lateAmount || 0}</td>
+                                                                                        <td colSpan={2}>{row.firstLateDate || '--'}</td>
+                                                                                        <td>{row.activity || '--'}</td>
+                                                                                        <td colSpan={3}>{row.address || '--'}</td>
+                                                                                        <td>{row.workArea || '--'}</td>
+                                                                                        <td colSpan={2}>{row.phoneNumber || '--'}</td>
+                                                                                    </tr>
+                                                                                    <tr><th>تعديل:</th></tr>
+                                                                                </React.Fragment>
+
+                                                                            ) // row return
+                                                                        }) // end of data map
+                                                                    }
+                                                                    <hr className="horizontal-line"></hr>
+                                                                    <div className = "row-nowrap">
+                                                                    <div className="row-nowrap"><p>إجمالي المنطقة :</p><p className="box">{area.areaName || '--'}</p></div>
+                                                                    <tr>
+                                                                        <th></th>
+                                                                        <th>عدد التمويلات</th>
+                                                                        <th>التمويل</th>
+                                                                        <th>عدد الاعضاء</th>
+                                                                   </tr>
+                                                                   <tr>
+                                                                       <td></td>
+                                                                       <td></td>
+                                                                       <td></td>
+                                                                       <td></td>
+                                                                   </tr>
+                                                                    </div>
+                                
+                                                                </tbody>
+                                                            </table>
+                                                        </React.Fragment>
+                                                    ) // area return
+                                                }) // end of areas map
+
+                                            }
+                                        </React.Fragment>
+
+                                    ) // officer return 
+                                }) // end of officers map
+                                }
+                            </React.Fragment>
+                        ) // branch return
+                    }) // end of response map
+
+                }
+            </div>
+        </>
+    )
 }
 
 export default ActiveWalletIndividual
