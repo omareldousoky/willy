d944da1f774d3bb2be326c5f9bd6d7360bdf06cf
Sara Alaa <sara3laa.93@gmail.com>
2021-02-25 17:43:20 +0200

    call financial closing api

--- src/Mohassel/Components/FinancialClosing/MonthlyClosing/monthlyClosing.tsx
+++ src/Mohassel/Components/FinancialClosing/MonthlyClosing/monthlyClosing.tsx
@@ -9,34 +9,60 @@ import { Col, Form, } from 'react-bootstrap';
 import BackButton from '../../BackButton/back-button';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+import { getErrorMessage } from '../../../../Shared/Services/utils';
+import { withRouter } from 'react-router-dom';
+
+interface Props {
+    history: Array<string>;
+}
 interface State {
     loading: boolean;
 }
 
-const endOfDay: Date = new Date();
+const today: Date = new Date();
 
 const monthClosingValidation = Yup.object().shape({
- closeDate : Yup.string().test('close date cant be in the future',local.dateCantBeInFuture,(value: string)=>{
-    return value ? new Date(value).valueOf() <= endOfDay.valueOf() : true;
- })
+    closeDate: Yup.string().test('close date cant be in the future', local.dateCantBeInFuture, (value: string) => {
+        return value ? new Date(value).valueOf() <= today.valueOf() : true;
+    })
 })
-class MonthlyClosing extends Component<{}, State>{
-    constructor(props) {
+class MonthlyClosing extends Component<Props, State>{
+    constructor(props: Props) {
         super(props);
         this.state = {
             loading: false,
-
         }
     }
     async Close(closeDate: number) {
+        this.setState({ loading: true })
+        console.log(closeDate)
         const res = await financialClosing({ closeDate })
         if (res.status == "success") {
+            this.setState({ loading: false })
             Swal.fire('Success', '', 'success');
+        } else {
+            this.setState({ loading: false })
+            Swal.fire('Error !', getErrorMessage(res.error.error), 'error');
         }
     }
-    async handleSubmit(values) {
+     handleSubmit = async(values) =>{
         const closeDate = values.closeDate;
-        console.log(new Date(closeDate).valueOf());
+        const endOfCloseDate = new Date(closeDate).setHours(23, 59, 59).valueOf();
+        console.log(endOfCloseDate);
+        Swal.fire({
+            title: local.areYouSure,
+            text: `${local.monthlyClosing}`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: local.monthlyClosing,
+            cancelButtonText: local.cancel
+        }).then(async (isConfirm) => {
+                if (isConfirm.value) {
+                    await this.Close(endOfCloseDate);
+                }
+            });
     }
     render() {
         return (
@@ -44,9 +70,9 @@ class MonthlyClosing extends Component<{}, State>{
                 <div className={'rowContainer'}>
                     <BackButton title={local.monthlyClosing} />
                 </div>
-                <Card style={{ margin: '20px 50px' }} className="print-none">
+                <Card className="main-card">
                     <Loader type="fullscreen" open={this.state.loading} />
-                    <Card.Body style={{ padding: 15, display: 'flex', justifyContent: 'center' }}>
+                    <Card.Body className=" d-flex justify-content-center">
                         <Formik
                             initialValues={{ closeDate: 0 }}
                             onSubmit={this.handleSubmit}
@@ -55,7 +81,7 @@ class MonthlyClosing extends Component<{}, State>{
                             validateOnChange
                         >
                             {(formikProps) =>
-                                <Form onSubmit={formikProps.handleSubmit}>
+                                <Form onSubmit={formikProps.handleSubmit} className="w-50">
                                     <Col sm={12} key={"colseDate"}>
                                         <Form.Group controlId="closeDate">
                                             <div
@@ -71,9 +97,8 @@ class MonthlyClosing extends Component<{}, State>{
                                                         textAlign: "center",
                                                     }}
                                                 >
-                                                    {local.date}
+                                                    {local.closeDate}
                                                 </p>
-                                                <span>{local.from}</span>
                                                 <Form.Control
                                                     style={{ marginLeft: 20, border: "none" }}
                                                     type="date"
@@ -119,9 +144,14 @@ class MonthlyClosing extends Component<{}, State>{
                             }
                         </Formik>
                     </Card.Body>
+                    <Card.Footer>
+                        <div className="d-flex">
+                            <p className="clickable-action" onClick={() => this.props.history.push('/reports')}>{local.reviewFinancialState}</p>
+                        </div>
+                    </Card.Footer>
                 </Card>
             </>
         )
     }
 }
-export default MonthlyClosing;
+export default withRouter(MonthlyClosing);
