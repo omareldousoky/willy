9af1d929407c0fa8feb39fd4a4cbfae774ebde37
radwa kamal <radwa.m.kamal@gmail.com>
2021-02-28 13:03:28 +0200

    Introduce icon font for checkbox/radio buttons & make bootstrap common file

--- src/Documents/index.tsx
+++ src/Documents/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'bootstrap/dist/css/bootstrap.css';
-import './custom.scss';
+import "../Shared/Assets/scss/app.scss";
 import { Provider } from 'react-redux'
 import store from '../Shared/redux/store';
 import App from './app';
