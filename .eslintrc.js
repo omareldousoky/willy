870f5b7ecdfee17b4f281b97e04521cfbb144787
radwa kamal <radwa.m.kamal@gmail.com>
2021-03-01 18:06:46 +0200

    Add prettier / stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,73 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-        'react',
-        'react-hooks'
-    ],
-    env: {
-        browser: true,
-        jest: true
-    },
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-    ],
-    parserOptions: {
-        project: [
-            "tsconfig.json",
-        ],
-        tsconfigRootDir: __dirname,
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        'react/jsx-filename-extension': ['warn', {extensions: ['.jsx', '.tsx']}],
-        'react/prop-types': 'off',
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
-    },
-    settings: {
-        react: {
-            version: 'detect'
-        }
-    }
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint',
+		'react',
+		'react-hooks',
+		'prettier'
+	],
+	env: {
+		browser: true,
+		jest: true
+	},
+	extends: [
+		'airbnb',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/typescript',
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		project: './tsconfig.json',
+		tsconfigRootDir: __dirname,
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		'global-require': 'off',
+		'prefer-template': 'off',
+		'no-unused-expressions': [
+			'warn',
+			{
+				'allowShortCircuit': true,
+				'allowTernary': true
+			}
+		],
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/prefer-interface': 'off',
+		'@typescript-eslint/no-useless-constructor': 'error',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'react/jsx-filename-extension': ['warn', {
+			extensions: ['.jsx', '.tsx']
+		}],
+		'react/prop-types': 'off',
+		'react/jsx-props-no-spreading': 'off',
+		'react/destructuring-assignment': 'off',
+		'react/jsx-one-expression-per-line': 'off',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'import/extensions': 'off',
+		'import/no-unused-imports': 'off', // no-unused-vars rule is sufficient
+		'import/prefer-default-export': 'off',
+		'import/no-named-as-default-member': 'off',
+		'import/no-unused-modules': [1, {
+			'unusedExports': true
+		}],
+		'prettier/prettier': [
+			'error',
+			{
+				'singleQuote': true,
+				'semi': false,
+				'indent': 2,
+				'trailingComma': 'es5'
+			}
+		]
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
 };
