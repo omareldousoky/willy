28a9305c688282fa907210735d4b50f412905c7f
radwa kamal <radwa.m.kamal@gmail.com>
2021-03-18 11:15:07 +0200

    fix: remove unused import

--- src/Mohassel/Components/dropDowns/allDropDowns.tsx
+++ src/Mohassel/Components/dropDowns/allDropDowns.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import AsyncSelect from 'react-select/async'
 import { useStore } from 'react-redux'
 import Select, { ValueType, Props } from 'react-select'
