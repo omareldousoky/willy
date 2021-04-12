c3ea0bef521675c884a95ca670c59d8ad039333b
Sara Alaa <sara3laa.93@gmail.com>
2021-04-12 20:35:50 +0200

    add clear icon for date field

--- src/Mohassel/Components/Common/FormikFields/dateField.tsx
+++ src/Mohassel/Components/Common/FormikFields/dateField.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React ,{useRef} from "react";
 import { FieldProps } from "formik";
-import { Col, FormControl, InputGroup } from "react-bootstrap";
+import {Col, FormControl, InputGroup } from "react-bootstrap";
 import * as local from "../../../../Shared/Assets/ar.json";
 
 interface DateFieldProps {
@@ -11,8 +11,8 @@ interface DateFieldProps {
   onlyField?: boolean;
   fieldClassName?: string;
   label?: string;
+  isClearable?: boolean;
 }
-
 const DateField = (props: DateFieldProps & FieldProps<string>) => {
   const {
     field,
@@ -25,10 +25,16 @@ const DateField = (props: DateFieldProps & FieldProps<string>) => {
     fieldClassName,
     onlyField = false,
     label,
+    isClearable,
     ...restProps
   } = props;
   const { touched, errors } = form;
-
+  const inputRef = useRef<HTMLInputElement>(null);
+ const onClear =() =>{
+   if(null !== inputRef?.current && inputRef.current.value){
+        inputRef.current.value='';
+   }
+  }
   return (
     <>
       {!onlyField && (
@@ -37,18 +43,25 @@ const DateField = (props: DateFieldProps & FieldProps<string>) => {
           className={`d-flex flex-column ${className || ""}`}
         >
           <InputGroup key={key}>
-            <InputGroup.Append>
+            <InputGroup.Prepend>
               <InputGroup.Text id={`${id || field.name}Text`}>
                 {label || local.date}
               </InputGroup.Text>
-            </InputGroup.Append>
+            </InputGroup.Prepend>
             <FormControl
+              ref={inputRef}
               type="date"
               {...field}
               {...restProps}
               id={id || field.name}
               className="mr-0"
             />
+            {isClearable &&
+            <InputGroup.Append>
+            <InputGroup.Text
+               onClick={onClear}><img className="w-75 h-75" src={require('../../../Assets/clear.svg')}/>
+               </InputGroup.Text></InputGroup.Append>
+            }
           </InputGroup>
           {touched[field.name] && errors[field.name] && (
             <small className="text-danger ml-auto mb-2">
