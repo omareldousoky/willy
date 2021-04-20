3db33d2e587b27238aa4c18320fac201369fe175
radwa kamal <radwa.m.kamal@gmail.com>
2021-04-20 14:30:21 +0200

    Add laundry reports

---
+++ src/Mohassel/Components/Reports/laundryReports.tsx
@@ -0,0 +1,11 @@
+import React, { Component, FunctionComponent } from 'react';
+import Card from 'react-bootstrap/Card';
+
+const LaundryReports: FunctionComponent = () => {
+	return (
+		<Card style={{ margin: '20px 50px' }} className="print-none">
+		</Card>
+	)
+}
+
+export default LaundryReports
