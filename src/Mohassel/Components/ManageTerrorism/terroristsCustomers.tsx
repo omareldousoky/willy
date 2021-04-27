e1e5a3bfe9673f0dbcaaefda50fde7102e94f593
Sara Alaa <sara3laa.93@gmail.com>
2021-04-27 06:27:03 +0200

    downloas as presignedUr

--- src/Mohassel/Components/ManageTerrorism/terroristsCustomers.tsx
+++ src/Mohassel/Components/ManageTerrorism/terroristsCustomers.tsx
@@ -3,18 +3,16 @@ import { Button } from 'react-bootstrap'
 import Swal from 'sweetalert2'
 import * as local from '../../../Shared/Assets/ar.json'
 import { Loader } from '../../../Shared/Components/Loader'
-import { DownloadAsCsv, getErrorMessage } from '../../../Shared/Services/utils'
+import {downloadFile, getErrorMessage } from '../../../Shared/Services/utils'
 import Can from '../../config/Can'
 import { downloadSuspectsReport } from '../../Services/APIs/Terrorism/terrorism'
-import { fullEnglishDate } from './terrorismInitials'
 export const TerroristsCustomers = () => {
   const [loading, setLoading] =useState(false)
   const downloadSuspectsList = async () => {
     setLoading(true)
-    const name = `suspectedTerroristsCustomers`
     const res = await downloadSuspectsReport()
-    if (res.status == 'success' && res.body) {
-          DownloadAsCsv(name,res.body)
+    if (res.status == 'success' && res.body.presignedUr) {
+          downloadFile(res.body.presignedUr)
     } else {
       Swal.fire('Error !', getErrorMessage(res.error.error),'error')
     }
