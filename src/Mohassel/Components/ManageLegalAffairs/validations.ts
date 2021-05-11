6e5a80cc625ca61fdebc0e71d4a43a3f37ac5222
Mohamed Abdelnaeem <melnaeem00@gmail.com>
2021-05-11 12:52:11 +0200

    SettlementForm: Support landline numbers

--- src/Mohassel/Components/ManageLegalAffairs/validations.ts
+++ src/Mohassel/Components/ManageLegalAffairs/validations.ts
@@ -5,7 +5,6 @@ export const defaultValidationSchema = Yup.string()
   .trim()
   .max(100, local.maxLength100)
 
-const EgyptPhoneNumberReGex = /^01[0125][0-9]{8}$/
 export const phoneNumberValidationSchema = Yup.string()
-  .trim()
-  .matches(EgyptPhoneNumberReGex, `${local.example}: 01234567891`)
+  .min(10, local.minLength10)
+  .max(11, local.maxLength11)
