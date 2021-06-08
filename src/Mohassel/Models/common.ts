8a551e3fff642d42f39bb2b3557ac1e837457f22
radwa kamal <radwa.m.kamal@gmail.com>
2021-06-08 15:38:19 +0200

    chore: Move trace to common

--- src/Mohassel/Models/common.ts
+++ src/Mohassel/Models/common.ts
@@ -3,3 +3,14 @@ export interface ApiResponse<T> {
   body?: T
   error?: unknown
 }
+
+export interface Signature {
+  by?: string
+  at?: number
+  userName?: string
+}
+
+export interface Trace {
+  created: Signature
+  updated: Signature
+}
