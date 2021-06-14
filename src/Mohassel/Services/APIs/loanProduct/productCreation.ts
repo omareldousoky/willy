9988d33ab7efce57c4c314a20f5c8dc1e5c74651
Sara Alaa <sara3laa.93@gmail.com>
2021-06-14 23:02:54 +0200

    use axios params

--- src/Mohassel/Services/APIs/loanProduct/productCreation.ts
+++ src/Mohassel/Services/APIs/loanProduct/productCreation.ts
@@ -3,7 +3,7 @@ import axios from '../axios-instance'
 const { REACT_APP_BASE_URL } = process.env
 const createProductUrl = `${REACT_APP_BASE_URL}/product/loan-product`
 const editProductUrl = `${REACT_APP_BASE_URL}/product/loan-product`
-const getProductApplicationsUrl = `${REACT_APP_BASE_URL}/report/product-applications?productId=`
+const getProductApplicationsUrl = `${REACT_APP_BASE_URL}/report/product-applications`
 export const createProduct = async (data: object) => {
   try {
     const res = await axios.post(createProductUrl, data)
@@ -22,8 +22,9 @@ export const editProduct = async (id: string, data: object) => {
 }
 
 export const getProductApplications = async (productId: string) => {
+  const params = { productId }
   try {
-    const res = await axios.get(`${getProductApplicationsUrl}${productId}`)
+    const res = await axios.get(getProductApplicationsUrl, { params })
     return { status: 'success', body: res.data }
   } catch (error) {
     return { status: 'error', error: error.response.data }
