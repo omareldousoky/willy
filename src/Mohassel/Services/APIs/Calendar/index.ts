543eac3b1f6d70e8a65e2e10b18e32a4e04f0c5f
HabibaGadalla <habiba.m.g@gmail.com>
2021-06-22 21:32:50 +0200

    feat: get calendar events api

---
+++ src/Mohassel/Services/APIs/Calendar/index.ts
@@ -0,0 +1,11 @@
+import axios from '../axios-instance'
+
+export const getCalendarEvents = async (data: object) => {
+  const url = process.env.REACT_APP_BASE_URL + `/search/legal-affairs-calendar`
+  try {
+    const res = await axios.post(url, data)
+    return { status: 'success', body: res.data }
+  } catch (error) {
+    return { status: 'error', error: error.response.data }
+  }
+}
