ab4c4c3dc312ed332ad56f2033f739d0f6925e9b
HabibaGadalla <habiba.m.g@gmail.com>
2021-06-23 17:21:38 +0200

    fix: arrows directions messed up

--- src/Mohassel/Components/LegalCalendar/Navigation.tsx
+++ src/Mohassel/Components/LegalCalendar/Navigation.tsx
@@ -4,11 +4,11 @@ import { LtsIcon } from '../../../Shared/Components'
 export const Navigation = ({ title, handlePrev, handleNext }) => {
   return (
     <div className="mx-3 text-secondary">
-      <span className="arrow mx-2" onClick={handlePrev}>
+      <span className="arrow mx-2" onClick={handleNext}>
         <LtsIcon name="arrow-left" />
       </span>
       <span className="week-number ">{title}</span>
-      <span className="arrow mx-2" onClick={handleNext}>
+      <span className="arrow mx-2" onClick={handlePrev}>
         <LtsIcon name="arrow-right" />
       </span>
     </div>
