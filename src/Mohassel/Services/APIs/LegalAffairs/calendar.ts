43d9182e6c45837e5efb54413aacb25cc4e62f96
HabibaGadalla <habiba.m.g@gmail.com>
2021-06-23 15:55:35 +0200

    fix; move api and add interface

--- src/Mohassel/Services/APIs/LegalAffairs/calendar.ts
+++ src/Mohassel/Services/APIs/LegalAffairs/calendar.ts
@@ -1,6 +1,9 @@
 import axios from '../axios-instance'
 
-export const getCalendarEvents = async (data: object) => {
+export const getCalendarEvents = async (data: {
+  sessionFromDate: number
+  sessionToDate: number
+}) => {
   const url = process.env.REACT_APP_BASE_URL + `/search/legal-affairs-calendar`
   try {
     const res = await axios.post(url, data)
