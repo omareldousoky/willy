5c50ef8da1eb83ec2aa6c8fdfb2bd90d2d76a577
Mohamed Abdelnaeem <melnaeem00@gmail.com>
2021-06-29 13:39:41 +0200

    Add local utilities for Max and Min validations message

--- src/Mohassel/localUtils.ts
+++ src/Mohassel/localUtils.ts
@@ -1,3 +1,8 @@
 import local from '../Shared/Assets/ar.json'
 
 export const missingKey = (key: string) => `${local[key]} غير موجود`
+
+export const maxValue = (value: number | string) =>
+  `لا يمكن ان يكون اكثر من ${value}`
+export const minValue = (value: number | string) =>
+  `لا يمكن ان يكون اقل من ${value}`
