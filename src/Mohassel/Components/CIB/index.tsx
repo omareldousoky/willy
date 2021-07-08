0dd11ac2627576fdada27d3d20191ad16f4debc7
radwa kamal <radwa.m.kamal@gmail.com>
2021-07-08 14:25:17 +0200

    Remove unused import

--- src/Mohassel/Components/CIB/index.tsx
+++ src/Mohassel/Components/CIB/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from 'react'
+import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import FormCheck from 'react-bootstrap/FormCheck'
