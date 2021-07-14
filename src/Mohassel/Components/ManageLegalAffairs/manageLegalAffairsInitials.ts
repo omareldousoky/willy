d4f3726cd562e02d1ae431bcde26de896c1f3c53
HabibaGadalla <habiba.m.g@gmail.com>
2021-07-14 11:10:37 +0200

    fix: change calendar icon

--- src/Mohassel/Components/ManageLegalAffairs/manageLegalAffairsInitials.ts
+++ src/Mohassel/Components/ManageLegalAffairs/manageLegalAffairsInitials.ts
@@ -22,7 +22,7 @@ export const manageLegalAffairsArray = (): Card[] => {
         path: '/legal-affairs/legal-actions',
       },
       {
-        icon: 'legal-actions',
+        icon: 'encoding-files',
         header: local.legalCalendar,
         desc: local.legalCalendar,
         path: '/legal-affairs/legal-calendar',
