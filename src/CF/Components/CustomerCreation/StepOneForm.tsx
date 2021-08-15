e37f88f79c7d89142e5bc9655c869738f1c2f0b0
Omar Eldousoky <omareldousoky@gmail.com>
2021-08-15 18:05:12 +0200

    changed mobile locale for cf customer

--- src/CF/Components/CustomerCreation/StepOneForm.tsx
+++ src/CF/Components/CustomerCreation/StepOneForm.tsx
@@ -564,7 +564,7 @@ export const StepOneForm = (props: any) => {
 
       <Form.Group controlId="mobilePhoneNumber">
         <Form.Label className="customer-form-label">
-          {local.mobilePhoneNumber}*
+          {local.cfMobileNumber}*
         </Form.Label>
         <Form.Control
           type="text"
