63d91aba37e178dad619cba1a187f8e5be51d665
Radwa Kamal <radwa.kamal@halan.com>
2021-08-23 11:16:19 +0200

    Convert lts loan list to functional component

---
+++ src/Mohassel/Components/LoanList/types.ts
@@ -0,0 +1,9 @@
+export interface LoanListProps {
+  branchId: string
+  fromBranch?: boolean
+}
+
+export interface LoanListLocationState {
+  id?: string
+  sme?: boolean
+}
---
+++ src/Mohassel/Components/LoanList/utils.ts
@@ -0,0 +1,7 @@
+export const chipStatusClass: Record<string, string> = {
+  paid: 'paid',
+  issued: 'unpaid',
+  pending: 'pending',
+  canceled: 'canceled',
+  default: '',
+}
