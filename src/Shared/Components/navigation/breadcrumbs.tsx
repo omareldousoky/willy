574ff3c7ceb2ab4c09b4380328419c8c6ad9ec70
Radwa Kamal <radwa.kamal@halan.com>
2021-08-23 19:25:47 +0200

    Fix breadcrumb displayed swapped

--- src/Shared/Components/navigation/breadcrumbs.tsx
+++ src/Shared/Components/navigation/breadcrumbs.tsx
@@ -11,18 +11,14 @@ const Breadcrumbs = ({ route }) => {
           crumb.path && (
             <React.Fragment key={index}>
               <div className="item">
-                {index < breadcrumbs.length - 1 ? (
-                  crumb.disableLink ? (
+                {index < breadcrumbs.length - 1 &&
+                  (crumb.disableLink ? (
                     crumb.label
                   ) : (
                     <NavLink to={crumb.path}>{crumb.label}</NavLink>
-                  )
-                ) : null}
+                  ))}
                 {index === breadcrumbs.length - 1 && crumb.label}
               </div>
-              {index < breadcrumbs.length - 1 && (
-                <LtsIcon name="arrow-left" size="12px" color="#afafaf" />
-              )}
             </React.Fragment>
           )
       )}
