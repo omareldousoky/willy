d85f83b2a88ec35712f37a8c65582af9cd3ae69a
Omar Eldousoky <omareldousoky@gmail.com>
2021-08-29 17:13:23 +0200

    fixed build error

--- src/Shared/Services/APIs/LoanUsage/getLoanUsage.ts
+++ src/Shared/Services/APIs/LoanUsage/getLoanUsage.ts
@@ -1,5 +1,5 @@
-import { API_BASE_URL } from '../../../../Shared/envConfig'
-import axios from '../../../../Shared/Services/axiosInstance'
+import { API_BASE_URL } from '../../../envConfig'
+import axios from '../../axiosInstance'
 
 export const getLoanUsage = async () => {
   const url = API_BASE_URL + `/config/usage`
