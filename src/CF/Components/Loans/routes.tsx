a48fe5508a256f0199947b112719fb26a2e1b5bc
Omar Eldousoky <omareldousoky@gmail.com>
2021-09-05 17:39:54 +0200

    fixed route permissions

--- src/CF/Components/Loans/routes.tsx
+++ src/CF/Components/Loans/routes.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import local from '../../../Shared/Assets/ar.json'
+import ability from '../../../Shared/config/ability'
 import { LoanList, LoanProfile, LoanRollBack } from '.'
 
 export const loansRoute = {
   path: '/loans',
   label: local.issuedLoans,
-  render: (props) => <LoanList {...props} />,
+  render: (props) =>
+    (ability.can('getIssuedLoan', 'application') ||
+      ability.can('branchIssuedLoan', 'application')) && (
+      <LoanList {...props} />
+    ),
   routes: [
     {
       path: '/loan-profile',
       label: local.loanDetails,
-      render: (props) => <LoanProfile {...props} />,
+      render: (props) =>
+        (ability.can('getIssuedLoan', 'application') ||
+          ability.can('branchIssuedLoan', 'application')) && (
+          <LoanProfile {...props} />
+        ),
     },
     {
       path: '/loan-roll-back',
