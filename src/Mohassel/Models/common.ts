749fda839e0a04ee6eac8b0608e9895381e24a0f
Sara Alaa <sara3laa.93@gmail.com>
2021-09-07 15:20:59 +0200

    merge prod

--- src/Mohassel/Models/common.ts
+++ src/Mohassel/Models/common.ts
@@ -1,26 +1,3 @@
-export interface ApiResponse<T> {
-  status: 'success' | 'error'
-  body?: T
-  error?: unknown
-}
-
-export interface SearchRequest {
-  from: number
-  size: number
-  order?: 'asc' | 'desc'
-  fromDate?: number
-  toDate?: number
-  name?: string
-  nationalId?: string
-  key?: number
-  url?: string // for FE ONLY
-  type?: 'sme' | 'micro' | 'nano'
-  beneficiaryType?: 'individual' | 'group'
-}
-
-export interface PaginatedResponse {
-  totalCount: number
-}
 export interface Product {
   id: string
   name: string
