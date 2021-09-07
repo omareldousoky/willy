93ed77d986c511eb9bf7b15d6c0283c388c0dc09
Omar Eldousoky <omareldousoky@gmail.com>
2021-09-07 15:05:20 +0200

    fix infinit calls to me API

--- src/Documents/app.tsx
+++ src/Documents/app.tsx
@@ -11,7 +11,7 @@ import './index.scss'
 const App = (props) => {
   useEffect(() => {
     props.getAuthData()
-  })
+  }, [props])
   if (getCookie('token') === '') {
     window.location.href = process.env.REACT_APP_LOGIN_URL || ''
     return <></>
