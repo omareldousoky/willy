aa684dea007230baba8c6c100b64b28bdaee1d8b
Radwa Kamal <radwa.kamal@halan.com>
2021-09-09 10:59:22 +0200

    WIP: cf early payment

--- src/CF/Components/Payment/index.tsx
+++ src/CF/Components/Payment/index.tsx
@@ -6,7 +6,10 @@ import { connect } from 'react-redux'
 import Button from 'react-bootstrap/Button'
 import DynamicTable from '../../../Shared/Components/DynamicTable/dynamicTable'
 import { Loader } from '../../../Shared/Components/Loader'
-import { manualPaymentValidation } from './paymentValidation'
+import {
+  earlyPaymentValidation,
+  manualPaymentValidation,
+} from './paymentValidation'
 import {
   getDateString,
   getErrorMessage,
@@ -28,6 +31,9 @@ import './styles.scss'
 import PaymentIcons from './paymentIcons'
 import ManualPayment from './manualPayment'
 import { LtsIcon } from '../../../Shared/Components'
+// import { getFirstDueInstallment } from '../../../Shared/Utils/payment'
+import { EarlyPayment } from './earlyPayment'
+import { calculateEarlyPayment } from '../../../Mohassel/Services/APIs/Payment'
 
 interface Props {
   installments: Array<Installment>
@@ -40,6 +46,7 @@ interface Props {
   setReceiptData: (data) => void
   print: (data) => void
   refreshPayment: () => void
+  setEarlyPaymentData: (data) => void
   // manualPaymentEditId: string
   paymentType: string
 }
@@ -57,6 +64,7 @@ interface State {
   loading: boolean
   loadingFullScreen: boolean
   remainingPrincipal: number
+  earlyPaymentFees: number
   requiredAmount: number
   installmentNumber: number
   // penalty: number
@@ -129,6 +137,7 @@ class Payment extends Component<Props, State> {
       loading: false,
       loadingFullScreen: false,
       remainingPrincipal: 0,
+      earlyPaymentFees: 0,
       requiredAmount: 0,
       installmentNumber: -1,
       payerType: '',
@@ -154,6 +163,29 @@ class Payment extends Component<Props, State> {
 
   componentWillUnmount() {
     this.props.changePaymentState(0)
+  }
+
+  async handleClickEarlyPayment() {
+    this.props.changePaymentState(2)
+    this.setState({ loading: true })
+    const res = await calculateEarlyPayment(this.props.applicationId)
+    if (res.status === 'success') {
+      this.props.setEarlyPaymentData({
+        remainingPrincipal: res.body.remainingPrincipal,
+        earlyPaymentFees: res.body.earlyPaymentFees,
+        requiredAmount: res.body.requiredAmount,
+      })
+      this.setState({
+        loading: false,
+        remainingPrincipal: res.body.remainingPrincipal,
+        earlyPaymentFees: res.body.earlyPaymentFees,
+        requiredAmount: res.body.requiredAmount,
+      })
+    } else {
+      this.setState({ loading: false }, () =>
+        Swal.fire('Error !', getErrorMessage(res.error.error), 'error')
+      )
+    }
   }
 
   getStatus(data) {
@@ -306,6 +338,7 @@ class Payment extends Component<Props, State> {
             paymentType={this.props.paymentType}
             application={this.props.application}
             installments={this.props.installments}
+            handleClickEarlyPayment={() => this.handleClickEarlyPayment()}
           />
         )
       case 1:
@@ -320,6 +353,78 @@ class Payment extends Component<Props, State> {
             paymentType={this.props.paymentType}
           />
         )
+      case 2:
+        return (
+          <Card className="payment-menu">
+            <Loader type="fullsection" open={this.state.loading} />
+            <div className="payment-info" style={{ textAlign: 'center' }}>
+              <LtsIcon name="early-payment" size="90px" color="#7dc255" />
+              <Button
+                variant="default"
+                onClick={() => this.props.changePaymentState(0)}
+              >
+                <span className="font-weight-bolder text-primary">&#8702;</span>
+                <small className="font-weight-bolder text-primary">
+                  &nbsp;{local.earlyPayment}
+                </small>
+              </Button>
+            </div>
+            <div className="verticalLine" />
+            <div style={{ width: '100%', padding: 20 }}>
+              <span
+                style={{
+                  cursor: 'pointer',
+                  float: 'left',
+                  background: '#E5E5E5',
+                  padding: 10,
+                  borderRadius: 15,
+                }}
+                onClick={() =>
+                  this.props.print({
+                    print: 'earlyPayment',
+                    remainingPrincipal: this.state.remainingPrincipal,
+                    earlyPaymentFees: this.state.earlyPaymentFees,
+                    requiredAmount: this.state.requiredAmount,
+                  })
+                }
+              >
+                <span
+                  className="fa fa-download"
+                  style={{ margin: '0px 0px 0px 5px' }}
+                />
+                {local.downloadPDF}
+              </span>
+              <Formik
+                enableReinitialize
+                initialValues={{
+                  ...this.state,
+                  max: this.props.application.installmentsObject
+                    .totalInstallments.installmentSum,
+                }}
+                onSubmit={this.handleSubmit}
+                validationSchema={earlyPaymentValidation}
+                validateOnBlur
+                validateOnChange
+              >
+                {(formikProps) => (
+                  <EarlyPayment
+                    loading={this.state.loading}
+                    application={this.props.application}
+                    formikProps={formikProps}
+                    installments={this.props.installments}
+                    remainingPrincipal={this.state.remainingPrincipal}
+                    earlyPaymentFees={this.state.earlyPaymentFees}
+                    requiredAmount={this.state.requiredAmount}
+                    setPayerType={(payerType: string) =>
+                      this.setState({ payerType })
+                    }
+                  />
+                )}
+              </Formik>
+            </div>
+          </Card>
+        )
+
       case 3:
         return (
           <Card className="payment-menu">
