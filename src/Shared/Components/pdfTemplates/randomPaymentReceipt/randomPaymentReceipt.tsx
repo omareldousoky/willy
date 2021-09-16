44ed1af0aa0ff0885bd6796db3c846d2097163e8
Omar Eldousoky <omareldousoky@gmail.com>
2021-09-16 15:52:27 +0200

    FIX BEBO CONFLICT

--- src/Shared/Components/pdfTemplates/randomPaymentReceipt/randomPaymentReceipt.tsx
+++ src/Shared/Components/pdfTemplates/randomPaymentReceipt/randomPaymentReceipt.tsx
@@ -4,39 +4,46 @@ import Tafgeet from 'tafgeetjs'
 import { numbersToArabic, extractGMTDate } from '../../../Services/utils'
 import local from '../../../Assets/ar.json'
 import { RandomPaymentReceiptProps } from './types'
-import { Header as CFHeader } from '../../../../CF/Components/PdfTemplates/pdfTemplatesCommon/header'
 
-const LTSReceiptHeader = () => (
-  <>
-    <table
-      className="w-100 text-center"
-      style={{
-        margin: '10px 0px',
-      }}
-    >
-      <tbody>
-        <tr style={{ height: '10px' }} />
-        <tr className="w-100 d-flex flex-row justify-content-between">
-          <th colSpan={6}>
-            <div className="logo-print-tb" />
-          </th>
-          <th colSpan={6}>
-            ترخيص ممارسة نشاط التمويل متناهي الصغر رقم (2) لسنة 2015
-          </th>
-        </tr>
-        <tr style={{ height: '10px' }} />
-      </tbody>
-    </table>
-  </>
-)
+const ReceiptHeader = (CF?: boolean) => {
+  return (
+    <>
+      <table
+        className="w-100 text-center"
+        style={{
+          margin: '10px 0px',
+        }}
+      >
+        <tbody>
+          <tr style={{ height: '10px' }} />
+          <tr className="w-100 d-flex flex-row justify-content-between">
+            <th colSpan={6}>
+              <div className={`${CF ? 'cf' : 'lts'}-logo-print-tb`} />
+            </th>
+            <th colSpan={6}>
+              {CF
+                ? 'ترخيص رقم 23 بتاريخ 31/05/2021'
+                : 'ترخيص ممارسة نشاط التمويل متناهي الصغر رقم (2) لسنه 2015'}
+            </th>
+          </tr>
+          <tr style={{ height: '10px' }} />
+        </tbody>
+      </table>
+      <div className="receipt-header">
+        <h5>{CF ? local.cfName : local.tasaheelName}</h5>
+        <h5>{local.paymentReceipt}</h5>
+      </div>
+    </>
+  )
+}
 
 const RandomPaymentReceipt = ({
   receiptData,
   appType = 'LTS',
 }: RandomPaymentReceiptProps) => {
   const pdfHeaders = {
-    LTS: <LTSReceiptHeader />,
-    CF: <CFHeader />,
+    LTS: ReceiptHeader(),
+    CF: ReceiptHeader(true),
   }
 
   return (
@@ -46,12 +53,6 @@ const RandomPaymentReceipt = ({
           <div key={index} className="random-payment-receipt" lang="ar">
             <div className="receipt-container">
               {pdfHeaders[appType]}
-
-              <div className="receipt-header">
-                <h5>{local.tasaheelName}</h5>
-                <h5>{local.paymentReceipt}</h5>
-              </div>
-
               <div className="receipt-content">
                 <div>
                   <span className="title">{local.date}</span>
