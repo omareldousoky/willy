d2d532300c80e5cc10b6b451a863e3e0868125d2
Sara Alaa <sara3laa.93@gmail.com>
2021-09-29 10:10:35 +0200

    call create lead api

--- src/Mohassel/Components/HalanIntegration/createLead.tsx
+++ src/Mohassel/Components/HalanIntegration/createLead.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Formik } from 'formik'
+import Swal from 'sweetalert2'
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
-import { Formik } from 'formik'
 import BackButton from '../../../Shared/Components/BackButton/back-button'
 import { Loader } from '../../../Shared/Components/Loader'
 import * as local from '../../../Shared/Assets/ar.json'
 import { LeadCreationForm } from './leadCreationForm'
 import { createLeadValidation, LeadCreationInitial } from './leadsValidation'
+import { createLead } from '../../../Shared/Services/APIs/Leads/createLead'
+import { getErrorMessage } from '../../../Shared/Services/utils'
+import { doneSuccessfully } from '../../../Shared/localUtils'
+import { LeadCore } from '../../../Shared/Models/common'
 
 export const CreateLead: React.FC = () => {
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+  const submit = async (values) => {
+    setLoading(true)
+    const leadData: LeadCore = { ...values }
+    const res = await createLead(leadData)
+    if (res.status === 'success') {
+      Swal.fire('', doneSuccessfully('createLead'), 'success')
+      history.goBack()
+    } else {
+      Swal.fire('', getErrorMessage(res.error.error), 'error')
+    }
+    setLoading(false)
+  }
   return (
     <div>
-      <Loader type="fullscreen" open={false} />
+      <Loader type="fullscreen" open={loading} />
       <BackButton title={local.createLead} />
       <Container>
         <Card>
@@ -19,9 +39,7 @@ export const CreateLead: React.FC = () => {
             <Formik
               initialValues={LeadCreationInitial}
               enableReinitialize
-              onSubmit={(values) => {
-                console.log(values)
-              }}
+              onSubmit={submit}
               validationSchema={createLeadValidation}
               validateOnBlur
               validateOnChange
--- src/Shared/Services/APIs/Leads/createLead.ts
+++ src/Shared/Services/APIs/Leads/createLead.ts
@@ -1,10 +1,11 @@
 import { API_BASE_URL } from '../../../envConfig'
+import { LeadCore } from '../../../Models/common'
 import axios from '../../axiosInstance'
 
-export const createLead = async (obj) => {
+export const createLead = async (leadData: LeadCore) => {
   const url = API_BASE_URL + '/lead'
   try {
-    const res = await axios.put(url, obj)
+    const res = await axios.post(url, leadData)
     return { status: 'success', body: res.data }
   } catch (error: any) {
     return { status: 'error', error: error.response.data }
