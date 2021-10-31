4bdd99cb4089bc7125f217cc0a742acf78f09d4f
Sara Alaa <sara3laa.93@gmail.com>
2021-10-31 16:13:12 +0200

    initialConsumerFinanceLimit min = 0

--- src/Shared/Validations/customer.ts
+++ src/Shared/Validations/customer.ts
@@ -101,7 +101,7 @@ export const customerCreationValidationStepOne = (
           }
         )
         .required(local.required),
-      otherwise: Yup.number().max(200000, maxValue(200000)),
+      otherwise: Yup.number().min(0).max(200000, maxValue(200000)),
     }),
     customerHomeAddress: Yup.string()
       .trim()
