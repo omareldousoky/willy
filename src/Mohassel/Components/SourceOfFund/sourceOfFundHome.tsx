03b9bffe6b9a8623d0ef5ec324f3af06a162fee9
karimmadnan <karim.adnan@halan.com>
2021-12-14 00:11:28 +0200

    addedCorrectTypes

--- src/Mohassel/Components/SourceOfFund/sourceOfFundHome.tsx
+++ src/Mohassel/Components/SourceOfFund/sourceOfFundHome.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode, useState } from 'react'
 import { CardNavBar, Tab } from 'Shared/Components/HeaderWithCards/cardNavbar'
-import Card from 'react-bootstrap/Card'
+import CardBT from 'react-bootstrap/Card'
+import { RouteComponentProps } from 'react-router-dom'
 import CIB from '../CIB'
 import SourceOfFund from './sourceOfFund'
 import * as local from '../../../Shared/Assets/ar.json'
 import HeaderWithCards from '../../../Shared/Components/HeaderWithCards/headerWithCards'
 import { manageLoansArray } from '../LoanList/manageLoansInitials'
 import ability from '../../config/ability'
+import { Card } from '../ManageAccounts/manageAccountsInitials'
 
 const tabsTemplate = [
   {
@@ -23,11 +25,9 @@ const tabsTemplate = [
   },
 ]
 
-const SourceOfFundHome: React.FC = (props) => {
-  const [activeTab, setActiveTab] = useState<string>('cibToTasaheel')
-  const [manageLoansHeader] = useState<Array<any>>(() => {
-    return manageLoansArray()
-  })
+const SourceOfFundHome: React.FC<RouteComponentProps> = (props) => {
+  const [activeTab, setActiveTab] = useState<string>('tasaheelToCib')
+  const [manageLoansHeader] = useState<Card[]>(manageLoansArray())
   const [tabs] = useState<Tab[]>(() => {
     const canTabs: Tab[] = []
     tabsTemplate.forEach(
@@ -52,7 +52,7 @@ const SourceOfFundHome: React.FC = (props) => {
   return (
     <>
       <HeaderWithCards
-        header={local.cib}
+        header={local.changeSourceOfFund}
         array={manageLoansHeader}
         active={manageLoansHeader
           .map((item) => {
@@ -60,7 +60,7 @@ const SourceOfFundHome: React.FC = (props) => {
           })
           .indexOf('change-source-of-fund')}
       />
-      <Card>
+      <CardBT>
         <CardNavBar
           array={tabs}
           active={activeTab}
@@ -69,7 +69,7 @@ const SourceOfFundHome: React.FC = (props) => {
           }}
         />
         {renderTab()}
-      </Card>
+      </CardBT>
     </>
   )
 }
