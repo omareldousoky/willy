731a49362296ccc2dee874730d5b3d4cb975afbb
karimmadnan <karim.adnan@halan.com>
2021-12-25 17:53:00 +0200

    classToFunctional

--- src/Mohassel/Components/Reports/cibReports.tsx
+++ src/Mohassel/Components/Reports/cibReports.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, FC } from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import Swal from 'sweetalert2'
@@ -6,6 +6,7 @@ import { Loader } from '../../../Shared/Components/Loader'
 import local from '../../../Shared/Assets/ar.json'
 import errorMessages from '../../../Shared/Assets/errorMessages.json'
 import {
+  getCibPortoReports,
   cibPaymentReport,
   getTpayFiles,
 } from '../../Services/APIs/Reports/cibPaymentReport'
@@ -31,34 +32,43 @@ interface TPAYFile {
   status: string
   url?: string
 }
-interface State {
-  data: Array<TPAYFile>
-  loading: boolean
-  showModal: boolean
-}
-class CIBReports extends Component<{}, State> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: false,
-      data: [],
-      showModal: false,
+
+const CIBReports: FC = () => {
+  const [data, setData] = useState<TPAYFile[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [showModal, setShowModal] = useState<boolean>(false)
+
+  const getCibReports = async () => {
+    setLoading(true)
+    const res = await getTpayFiles()
+    if (res.status === 'success' && res.body) {
+      setLoading(false)
+      setData(res.body.cibFile ? res.body.cibFile : [])
+    } else {
+      setLoading(false)
+      Swal.fire('error', local.searchError, 'error')
     }
   }
 
-  componentDidMount() {
-    this.getCibReports()
-  }
+  useEffect(() => {
+    getCibReports()
+    getCibPortoReports({
+      branches: ['5efa45c1b7a37f7666b8d35a'],
+      endDate: 1639087199999,
+      startDate: 1634508000000,
+    })
+  }, [])
 
-  async handleSubmit(values) {
+  const handleSubmit = async (values) => {
     const date = new Date(values.date).setHours(23, 59, 59, 999).valueOf()
-    this.setState({ loading: true, showModal: false })
+    setLoading(true)
+    setShowModal(false)
     const res = await cibPaymentReport({ endDate: date })
     if (res.status === 'success') {
-      this.setState({ loading: false })
+      setLoading(false)
       if (res.body.status && res.body.status === 'processing') {
         Swal.fire('', local.fileQueuedSuccess, 'success').then(() =>
-          this.getCibReports()
+          getCibReports()
         )
       } else {
         const link = document.createElement('a')
@@ -69,132 +79,116 @@ class CIBReports extends Component<{}, State> {
         link.remove()
       }
     } else {
-      this.setState({ loading: false })
+      setLoading(false)
       Swal.fire('', local.fileQueuedError, 'error')
     }
   }
 
-  async getCibReports() {
-    this.setState({ loading: true })
-    const res = await getTpayFiles()
-    if (res.status === 'success' && res.body) {
-      this.setState({
-        data: res.body.cibFile ? res.body.cibFile : [],
-        loading: false,
-      })
-    } else {
-      this.setState({ loading: false })
-      Swal.fire('error', local.searchError, 'error')
-    }
-  }
-
-  async getFileUrl(fileKey: string) {
-    this.setState({ loading: true })
+  const getFileUrl = async (fileKey: string) => {
+    setLoading(true)
     const res = await cibTpayURL(fileKey)
     if (res.status === 'success') {
-      this.setState({ loading: false })
+      setLoading(false)
       downloadFile(res.body.url)
     } else {
-      this.setState({ loading: false }, () =>
-        Swal.fire('', errorMessages.doc_read_failed.ar, 'error')
-      )
+      setLoading(false)
+      Swal.fire('', errorMessages.doc_read_failed.ar, 'error')
     }
   }
 
-  render() {
-    return (
-      <>
-        <Card style={{ margin: '20px 50px' }} className="print-none">
-          <Loader type="fullscreen" open={this.state.loading} />
-          <Card.Body style={{ padding: 15 }}>
-            <div className="custom-card-header">
-              <Card.Title style={{ marginLeft: 20, marginBottom: 0 }}>
-                {local.cibReports}
-              </Card.Title>
-              <Can I="cibScreen" a="report">
-                <Button
-                  type="button"
-                  variant="primary"
-                  onClick={() => this.setState({ showModal: true })}
-                >
-                  {local.newRequest}
-                </Button>
-              </Can>
-            </div>
-            {this.state.data.length > 0 ? (
-              this.state.data.map((pdf, index) => {
-                return (
-                  <Card key={index}>
-                    <Card.Body>
-                      <div className="d-flex justify-content-between font-weight-bold">
-                        <div className="d-flex">
-                          <span className="mr-5 text-secondary">
-                            #{index + 1}
-                          </span>
-                          <span className="mr-5 d-flex flex-start flex-column">
-                            <span>{local.loanAppCreationDate}</span>
-                            {timeToArabicDate(pdf.created.at, true)}
-                          </span>
-                          <span className="mr-5">{pdf.key.split('/')[1]}</span>
-                          <span
-                            className={`mr-5  text-${
-                              pdf.status === 'created'
-                                ? 'success'
-                                : pdf.status === 'processing'
-                                ? 'warning'
-                                : 'danger'
-                            } `}
-                          >
-                            {getIscoreReportStatus(pdf.status)}
-                          </span>
-                          {pdf.status === 'created' && (
-                            <span className="mr-5 d-flex flex-start flex-column">
-                              <span>{local.creationDate}</span>
-                              {timeToArabicDate(pdf.created?.at, true)}
-                            </span>
-                          )}
-                        </div>
+  return (
+    <>
+      <Card style={{ margin: '20px 50px' }} className="print-none">
+        <Loader type="fullscreen" open={loading} />
+        <Card.Body style={{ padding: 15 }}>
+          <div className="custom-card-header">
+            <Card.Title style={{ marginLeft: 20, marginBottom: 0 }}>
+              {local.cibReports}
+            </Card.Title>
+            <Can I="cibScreen" a="report">
+              <Button
+                type="button"
+                variant="primary"
+                onClick={() => setShowModal(true)}
+              >
+                {local.newRequest}
+              </Button>
+            </Can>
+          </div>
+          {data.length > 0 ? (
+            data.map((pdf, index) => {
+              return (
+                <Card key={index}>
+                  <Card.Body>
+                    <div className="d-flex justify-content-between font-weight-bold">
+                      <div className="d-flex">
+                        <span className="mr-5 text-secondary">
+                          #{index + 1}
+                        </span>
+                        <span className="mr-5 d-flex flex-start flex-column">
+                          <span>{local.loanAppCreationDate}</span>
+                          {timeToArabicDate(pdf.created.at, true)}
+                        </span>
+                        <span className="mr-5">{pdf.key.split('/')[1]}</span>
+                        <span
+                          className={`mr-5  text-${
+                            pdf.status === 'created'
+                              ? 'success'
+                              : pdf.status === 'processing'
+                              ? 'warning'
+                              : 'danger'
+                          } `}
+                        >
+                          {getIscoreReportStatus(pdf.status)}
+                        </span>
                         {pdf.status === 'created' && (
-                          <Button
-                            type="button"
-                            variant="default"
-                            onClick={() => this.getFileUrl(pdf.key)}
-                            title="download"
-                          >
-                            <LtsIcon
-                              name="download"
-                              color="#7dc356"
-                              size="40px"
-                            />
-                          </Button>
+                          <span className="mr-5 d-flex flex-start flex-column">
+                            <span>{local.creationDate}</span>
+                            {timeToArabicDate(pdf.created?.at, true)}
+                          </span>
                         )}
                       </div>
-                    </Card.Body>
-                  </Card>
-                )
-              })
-            ) : (
-              <div className="d-flex align-items-center justify-content-center">
-                {local.noResults}{' '}
-              </div>
-            )}
-          </Card.Body>
-        </Card>
-        {this.state.showModal && (
-          <ReportsModal
-            pdf={{
-              key: 'cibPaymentReport',
-              local: 'سداد اقساط CIB',
-              inputs: ['date'],
-              permission: 'cibScreen',
-            }}
-            show={this.state.showModal}
-            hideModal={() => this.setState({ showModal: false })}
-            submit={(values) => this.handleSubmit(values)}
-          />
-        )}
-      </>
-    )
-  }
+                      {pdf.status === 'created' && (
+                        <Button
+                          type="button"
+                          variant="default"
+                          onClick={() => getFileUrl(pdf.key)}
+                          title="download"
+                        >
+                          <LtsIcon
+                            name="download"
+                            color="#7dc356"
+                            size="40px"
+                          />
+                        </Button>
+                      )}
+                    </div>
+                  </Card.Body>
+                </Card>
+              )
+            })
+          ) : (
+            <div className="d-flex align-items-center justify-content-center">
+              {local.noResults}{' '}
+            </div>
+          )}
+        </Card.Body>
+      </Card>
+      {showModal && (
+        <ReportsModal
+          pdf={{
+            key: 'cibPaymentReport',
+            local: 'سداد اقساط CIB',
+            inputs: ['date'],
+            permission: 'cibScreen',
+          }}
+          show={showModal}
+          hideModal={() => setShowModal(false)}
+          submit={(values) => handleSubmit(values)}
+        />
+      )}
+    </>
+  )
 }
+
 export default CIBReports
--- src/Mohassel/Services/APIs/Reports/cibPaymentReport.ts
+++ src/Mohassel/Services/APIs/Reports/cibPaymentReport.ts
@@ -5,6 +5,12 @@ interface ChangeFundObj {
   endDate: number
 }
 
+interface CibPortoReport {
+  startDate?: number
+  endDate: number
+  branches: string[]
+}
+
 export const cibPaymentReport = async (data: ChangeFundObj) => {
   const url = API_BASE_URL + `/report/cib-payments`
   try {
@@ -19,6 +25,16 @@ export const getTpayFiles = async () => {
   const url = API_BASE_URL + `/report/tpay-files`
   try {
     const res = await axios.get(url, { params: {} })
+    return { status: 'success', body: res.data }
+  } catch (error) {
+    return { status: 'error', error: error.response.data }
+  }
+}
+
+export const getCibPortoReports = async (data: CibPortoReport) => {
+  const url = API_BASE_URL + `/report/excel/cib-portfolio-securitization`
+  try {
+    const res = await axios.post(url, data)
     return { status: 'success', body: res.data }
   } catch (error) {
     return { status: 'error', error: error.response.data }
