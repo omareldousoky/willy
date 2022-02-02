e2bc84ed178a6b99b4509eb78c214f055754465e
karimmadnan <karim.adnan@halan.com>
2022-02-02 16:40:03 +0200

    FLContract

---
+++ src/Shared/Components/pdfTemplates/FinancialLeasingContract/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+import Tafgeet from 'tafgeetjs'
+import * as local from 'Shared/Assets/ar.json'
+import {
+  numbersToArabic,
+  timeToArabicDate,
+  dayToArabic,
+  getIndexInArabic,
+  getNumbersOfGuarantor,
+  getIndexOfGuarantorInAr,
+} from 'Shared/Services/utils'
